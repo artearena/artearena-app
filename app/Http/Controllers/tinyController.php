eb55056f0f185f8ed39696df7a11dc1ae6098ffd
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-09-25 16:37:57 -0300

    subindo primeira versão tiny

--- app/Http/Controllers/tinyController.php
+++ app/Http/Controllers/tinyController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\tiny;
+use PDF; 
 
 class tinyController extends Controller
 {
+
     public function relatorio()
     {
-        $tiny = tiny::all();
-        return view('pages.tiny.relatorio', compact('tiny'));
+        $tiny = tiny::selectRaw('SUM(total_pedido) AS total_pedido, SUM(valor_frete) AS total_frete, id_vendedor')
+        ->groupBy('id_vendedor')
+        ->get();
+        
+        $pdf = PDF::loadView('relatorio', compact('tiny'));
+        return $pdf->download('relatorio-tiny.pdf');
     }
+
 }
--- resources/views/pages/tiny/relatorio.blade.php
+++ resources/views/pages/tiny/relatorio.blade.php
@@ -1,12 +1,33 @@
 @extends('layout.main')
-
 @section('title')
     Relatório Trello
 @endsection
-
 @section('style')
-
+    <style>
+        /* Estilos personalizados para a tela de relatórios */
+    </style>
 @endsection
 @section('body')
+    <div class="container">
+        <h1>Relatório Trello</h1>
+        <table>
+            <thead>
+                <tr>
+                <th>Vendedor</th>
+                <th>Total Pedido</th>
+                <th>Total Frete</th> 
+                </tr>
+            </thead>
 
+            <tbody>
+                @foreach($tiny as $item)
+                <tr>
+                    <td>{{ $item->id_vendedor }}</td>
+                    <td>R$ {{ number_format($item->total_pedido, 2, ',', '.') }}</td>
+                    <td>R$ {{ number_format($item->total_frete, 2, ',', '.') }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>    
+    </div>
 @endsection
