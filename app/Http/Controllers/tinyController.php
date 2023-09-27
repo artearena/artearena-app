27ab18c9958f24560ebe4aa820e7b8d1b378b0ef
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-09-27 11:30:35 -0300

    tiny

--- app/Http/Controllers/tinyController.php
+++ app/Http/Controllers/tinyController.php
@@ -5,28 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\tiny;
 use App\Models\Usuario;
-use PDF; 
+use PDF;
 
-class tinyController extends Controller
+class TinyController extends Controller
 {
-
     public function gerarPdf()
     {
-        $dados = tiny::All();
-        
+        $dados = tiny::all();
         $pdf = PDF::loadView('relatorioPdf', compact('dados'));
         return $pdf->download('relatorio.pdf');
     }
 
-    public function exibirRelatorio() 
+    public function exibirRelatorio()
     {
-        $dados = tiny::selectRaw('SUM(total_pedido) AS total_pedido, 
-            SUM(valor_frete) AS total_frete, 
-            id_vendedor')
-        ->groupBy('id_vendedor')
-        ->get();
-        $vendedores = Usuarios::whereNotNull('id_vendedor')->pluck('nome_usuario');
+        $dados = tiny::selectRaw('SUM(total_pedido) AS total_pedido, SUM(valor_frete) AS total_frete, id_vendedor')
+            ->groupBy('id_vendedor')
+            ->get();
 
-        return view('pages.tiny.relatorio', compact('dados', 'vendedores')); 
+        $vendedores = Usuario::whereNotNull('id_vendedor')->pluck('nome_usuario');
+        return view('pages.tiny.relatorio', compact('dados', 'vendedores'));
     }
 }
