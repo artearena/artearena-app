2c1be3e75a432d184526a2ca353f9eb0f7ab71dd
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-09-27 13:07:01 -0300

    subir att

--- resources/views/pages/tiny/relatorio.blade.php
+++ resources/views/pages/tiny/relatorio.blade.php
@@ -4,29 +4,36 @@
 @endsection
 @section('style')
     <style>
-        /* Estilos CSS */
+
         table {
             width: 100%;
             border-collapse: collapse; 
         }
+
         th, td {
             padding: 8px;
             text-align: left;
             border-bottom: 1px solid #ddd;
         }
+
         th {
             background-color: #f2f2f2;
             font-weight: bold;
         }
+
         td {
             font-size: .9em;
         }
+
         tfoot td {
             font-weight: bold;
         }
+
         tbody tr:hover {
             background-color: #f5f5f5;
         }
+
+
     </style>
 @endsection
 @section('content')
@@ -41,21 +48,24 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach($dados as $item)
+            @foreach($dados as $item)
                 <tr>
-                    <td>{{ $vendedores[$item->id_vendedor] ?? 'Vendedor não encontrado' }}</td>
+                    @php
+                        $vendedor = $vendedores->firstWhere('id_vendedor', $item->id_vendedor);
+                    @endphp
+                    <td>{{ $vendedor ? $vendedor->nome_usuario : 'Sem Vendedor' }}</td>
                     <td>R$ {{ number_format($item->total_pedido, 2, ',', '.') }}</td>
                     <td>R$ {{ number_format($item->total_frete, 2, ',', '.') }}</td>
                 </tr>
-                @endforeach
-            </tbody>
-            <tfoot>
+                <tfoot>
                 <tr>
                     <td><strong>Total</strong></td>
                     <td><strong>R$ {{ number_format($dados->sum('total_pedido'), 2, ',', '.') }}</strong></td>
                     <td><strong>R$ {{ number_format($dados->sum('total_frete'), 2, ',', '.') }}</strong></td>
                 </tr>
             </tfoot>
+            @endforeach
+            </tbody>
         </table>
     </div>
 @endsection
