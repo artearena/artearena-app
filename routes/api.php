5ed6372b1641035208059dcaac1a226e4b2dfdec
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-09-27 13:34:40 -0300

    api updta

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,10 @@ use App\Http\Controllers\Api\OctaController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/cadastrar', [OctaController::class, 'cadastrar']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/cadastrar', [OctaController::class, 'cadastrar']);
+
