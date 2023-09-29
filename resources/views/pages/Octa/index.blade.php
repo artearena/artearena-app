b47800d98f3557a4ee12fcacd8a3e0bb3ee33f52
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-09-29 15:06:12 -0300

    teste

--- resources/views/pages/Octa/index.blade.php
+++ resources/views/pages/Octa/index.blade.php
@@ -4,7 +4,32 @@
 @endsection 
 @section('style') 
     <style> 
-
+        table { 
+            width: 100%; 
+            border-collapse: collapse; 
+        } 
+        th, td { 
+            padding: 8px; 
+            border-bottom: 1px solid #ddd; 
+            text-align: center; 
+        } 
+        th { 
+            background-color: #f2f2f2; 
+            font-weight: bold; 
+            text-align: center; 
+        } 
+        td { 
+            font-size: .9em; 
+        } 
+        tfoot td { 
+            font-weight: bold; 
+        } 
+        tbody tr:nth-child(even) { 
+            background-color: #f5f5f5; 
+        } 
+        tbody tr:hover { 
+            background-color: #ebebeb; 
+        } 
     </style> 
 @endsection 
 @section('content') 
