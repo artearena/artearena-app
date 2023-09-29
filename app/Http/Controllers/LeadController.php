8e71cab7dae6e3bf818bbe4d7733871951b9260d
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-09-29 16:26:02 -0300

    up

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cliente;
-use App\Models\Mensagem;
+use App\Models\TemplateMensagem;
 
 class LeadController extends Controller
 {
