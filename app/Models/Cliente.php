af33780ea0fe37356faea67d929424d0049fa8e5
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-09-29 16:33:23 -0300

    sad

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\TemplateMensagem;
+use App\Models\TemplateMensagem;
 
 class Cliente extends Model
 {
