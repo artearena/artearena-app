bc0cbf6f8605e2e19d523340ebe764642a42da30
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-09-29 09:54:14 -0300

    sa

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,13 +4,13 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
      */
-    use Illuminate\Support\Facades\Log;
 
     protected function schedule(Schedule $schedule): void
     {
