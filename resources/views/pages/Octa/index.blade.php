b61886c6e767b384b41e9c336a0df637126bf515
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-02 02:24:54 -0300

    att

--- resources/views/pages/Octa/index.blade.php
+++ resources/views/pages/Octa/index.blade.php
@@ -116,6 +116,8 @@
     <script>
         $(document).ready(function() {
             $('#clientesTable').DataTable({
+                searching: false, // Desabilita a funcionalidade de pesquisa
+                info: false, // Desabilita a exibição de informações sobre
                 language: {
                     url: 'https://cdn.datatables.net/plug-ins/1.10.24/i18n/Portuguese-Brasil.json'
                 }
