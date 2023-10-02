9c19117b223ce0392749015b382995805532003c
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-02 02:07:58 -0300

    ok

--- resources/views/pages/Octa/index.blade.php
+++ resources/views/pages/Octa/index.blade.php
@@ -135,7 +135,7 @@
                 // Enviar requisição AJAX para atualizar a tabela com os novos dados
                 $.ajax({
                     url: '/crm/atualizar-data',
-                    method: 'POST',
+                    method: 'PUT',
                     data: {
                         id: id,
                         newDateTime: newDateTime,
--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::middleware(['auth'])->group(function () {
     Route::prefix('crm')->group(function () {
         Route::any('/', [LeadController::class, 'index'])->name('octa.crm');
         Route::put('/{id}', [LeadController::class, 'update'])->name('octa.update');
-        Route::post('/atualizar-data/{id}', [LeadController::class, 'atualizarData']);
+        Route::put('/atualizar-data/{id}', [LeadController::class, 'atualizarData']);
         Route::post('/atualizar-mensagem', [LeadController::class, 'atualizarMensagem'])->name('octa.atualizarMensagem');
 
     });
