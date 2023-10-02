ad940689a2e4bd6935158eb8ab22134e302a14a0
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-02 13:12:14 -0300

    att

--- resources/views/pages/Octa/index.blade.php
+++ resources/views/pages/Octa/index.blade.php
@@ -138,7 +138,7 @@
             $('.datetimepicker').on('change', function() {
 
                 var id = $(this).closest('tr').find('.cliente-id').text();
-                var newDateTime = $(this).closest('tr').find('#date').val();
+                var newDateTime = $(this).val();
 
                 console.log(newDateTime);
                 // Enviar requisição AJAX para atualizar a tabela com os novos dados
