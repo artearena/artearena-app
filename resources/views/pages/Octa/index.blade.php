b3dfd4950dee085af57e2d37003dff7647d8698e
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-02 01:43:33 -0300

    tr

--- resources/views/pages/Octa/index.blade.php
+++ resources/views/pages/Octa/index.blade.php
@@ -114,6 +114,8 @@
     </div>
 @endsection
 @section('extraScript')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
 
     <script>
         $(document).ready(function() {
