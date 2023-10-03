4c4b944134504d1f6da725945447db9544a7b61e
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-03 13:21:41 -0300

    att home

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,98 +11,81 @@ Inicio
 <div class="container">
     <div class="row">
         <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Bandeiras</h5>
-                    <p class="card-text">Descrição da funcionalidade "Bandeiras"</p>
-                    <a href="{{ route('bandeira') }}" class="btn btn-outline-success">Acessar</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Gerar Orçamentos</h5>
-                    <p class="card-text">Descrição de Gerar Orçamentos</p>
-                    <a href="{{ route('frete') }}" class="btn btn-outline-warning">Acessar</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Arte Final</h5>
-                    <p class="card-text">Descrição de arte final</p>
-                    <a href="{{ route('pedido') }}" class="btn btn-outline-info">Acessar</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row mt-4">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Impressão</h5>
-                    <p class="card-text">Descrição de impressão</p>
-                    <a href="{{ route('impressao') }}" class="btn btn-outline-primary">Acessar</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Controle de Confecções</h5>
-                    <p class="card-text">Descrição de controle de confecção</p>
-                    <a href="{{ route('confeccao') }}" class="btn btn-outline-dark">Acessar</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Controle de Reposições</h5>
-                    <p class="card-text">Descrição de controle de confecção</p>
-                    <a href="{{ route('reposicao') }}" class="btn btn-outline-danger">Acessar</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row mt-4">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Encurtar Link</h5>
-                    <p class="card-text">Descrição de encurtar link</p>
-                    <button id="btn-encurtar-link" class="btn btn-outline-secondary">Encurtar Link</button>                
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Tiny Relatório</h5>
-                    <p class="card-text">Descrição de controle de confecção</p>
-                    <a href="{{ route('tiny.relatorio') }}" class="btn btn-outline-danger">Acessar</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Mini Trello</h5>
-                    <p class="card-text">Descrição de controle de confecção</p>
-                    <a href="{{ route('trello.index') }}" class="btn btn-outline-dark">Acessar</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row mt-4">
-        <div class="col-md-4">
+            <h5>Comercial</h5>
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">CRM Octa</h5>
-                    <p class="card-text">Descrição de controle de confecção</p>
-                    <a href="{{ route('octa.crm') }}" class="btn btn-outline-dark">Acessar</a>
+                    <p class="card-text">Descrição de CRM Octa</p>
+                    <a href="{{ route('crm') }}" class="btn btn-outline-success">Acessar</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Bandeira</h5>
+                    <p class="card-text">Descrição de Bandeira</p>
+                    <a href="{{ route('bandeira') }}" class="btn btn-outline-warning">Acessar</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Gerar Orçamento</h5>
+                    <p class="card-text">Descrição de Gerar Orçamento</p>
+                    <a href="{{ route('orcamento') }}" class="btn btn-outline-info">Acessar</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Encurtar Link</h5>
+                    <p class="card-text">Descrição de Encurtar Link</p>
+                    <button id="btn-encurtar-link" class="btn btn-outline-primary">Encurtar Link</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <h5>Design</h5>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Arte Final</h5>
+                    <p class="card-text">Descrição de Arte Final</p>
+                    <a href="{{ route('arte-final') }}" class="btn btn-outline-success">Acessar</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Tiny Relatório</h5>
+                    <p class="card-text">Descrição de Tiny Relatório</p>
+                    <a href="{{ route('tiny-relatorio') }}" class="btn btn-outline-warning">Acessar</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Mini Trello</h5>
+                    <p class="card-text">Descrição de Mini Trello</p>
+                    <a href="{{ route('mini-trello') }}" class="btn btn-outline-info">Acessar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <h5>Produção</h5>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Impressão</h5>
+                    <p class="card-text">Descrição de Impressão</p>
+                    <a href="{{ route('impressao') }}" class="btn btn-outline-primary">Acessar</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Confecção</h5>
+                    <p class="card-text">Descrição de Confecção</p>
+                    <a href="{{ route('confeccao') }}" class="btn btn-outline-dark">Acessar</a>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Reposição</h5>
+                    <p class="card-text">Descrição de Reposição</p>
+                    <a href="{{ route('reposicao') }}" class="btn btn-outline-danger">Acessar</a>
                 </div>
             </div>
         </div>
