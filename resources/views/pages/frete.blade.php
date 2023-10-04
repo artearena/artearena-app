5641501f26ea4f10b0aa8adeeaa353d18b3c22cb
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-04 16:39:43 -0300

    update retirada

--- resources/views/pages/frete.blade.php
+++ resources/views/pages/frete.blade.php
@@ -376,7 +376,85 @@ Simulação de Frete
         }
         console.log(data);
         data.forEach(transportadora => {
-          if (transportadora.transp_nome !== "Retira") {
+          if (transportadora.transp_nome === "Retirada") {
+            const cardElement = document.createElement("div");
+            cardElement.classList.add("card");
+            const titulo = document.createElement("p");
+            const nomeElement = document.createElement("h4");
+            nomeElement.textContent = "Retirada";
+            titulo.appendChild(nomeElement);
+            const logoElement = document.createElement("img");
+            logoElement.src = transportadora.url_logo;
+            const valorFreteElement = document.createElement("p");
+            valorFreteElement.textContent = `Frete Grátis`;
+            const prazoEntregaElement = document.createElement("p");
+            prazoEntregaElement.textContent = `Prazo de Entrega: Imediato`;
+            const dataPrevEntregaElement = document.createElement("p");
+            dataPrevEntregaElement.textContent = `Previsão: ${formatarData(transportadora.dtPrevEnt)}`;
+            cardElement.appendChild(titulo);
+            cardElement.appendChild(logoElement);
+            cardElement.appendChild(valorFreteElement);
+            cardElement.appendChild(prazoEntregaElement);
+            cardElement.appendChild(dataPrevEntregaElement);
+            cardsContainer.appendChild(cardElement);
+            // Adicionar evento de seleção ao card
+            cardElement.addEventListener("click", function() {
+              const selectedCard = document.querySelector(".card.selected");
+              if (selectedCard) {
+                selectedCard.classList.remove("selected");
+              }
+              // Adicionar classe "selected" ao card selecionado
+              this.classList.add("selected");
+              // Exibir detalhes do frete no campo de texto
+              const campoTexto = document.getElementById("campoTexto");
+              campoTexto.value = "";
+              let produtosSelecionados = {};
+              const tableRows = $("#produtoTableBody tr");
+              tableRows.each(function() {
+                const id = $(this).find("td:first-child").text();
+                const nomeProduto = $(this).find("td:nth-child(2)").text();
+                const valorProduto = parseFloat($(this).find("td:nth-child(3) input").val());
+                const quantidade = parseInt($(this).find("td:nth-child(5) input").val());
+                if (!produtosSelecionados.hasOwnProperty(id)) {
+                  produtosSelecionados[id] = {
+                    nome: nomeProduto,
+                    valor: valorProduto,
+                    quantidade: quantidade
+                  };
+                } else {
+                  produtosSelecionados[id].quantidade += quantidade;
+                }
+              });
+              let produtosDescricao = "";
+              for (const id in produtosSelecionados) {
+                if (produtosSelecionados.hasOwnProperty(id)) {
+                  const nomeProduto = produtosSelecionados[id].nome;
+                  const quantidade = produtosSelecionados[id].quantidade;
+                  const valor = produtosSelecionados[id].valor;
+                  const produtoDescricao = `${quantidade} un - ${nomeProduto} - R$${valor}\n`;
+                  produtosDescricao += produtoDescricao;
+                }
+              }
+              const titulo = "Retirada";
+              const frete = 0;
+              const prazoEntrega = "Imediato";
+              let valorTotal = 0;
+              for (const id in produtosSelecionados) {
+                if (produtosSelecionados.hasOwnProperty(id)) {
+                  const valorProduto = produtosSelecionados[id].valor;
+                  const quantidade = produtosSelecionados[id].quantidade;
+                  valorTotal += valorProduto * quantidade;
+                }
+              }
+              valorTotal += parseFloat(frete);
+              const valorTotalFormatado = valorTotal.toFixed(2);
+              const prazoConfeccao = prazoConfecaoMaisAlto;
+              const detalhesFrete = `Retirada Grátis\n\n`;
+              const total = `Total: R$${valorTotalFormatado}\n`;
+              const prazo = `Prazo para confecção é de ${prazoConfeccao} dias úteis.\nPrazo inicia-se após aprovação da arte e pagamento confirmado`;
+              campoTexto.value = `${produtosDescricao}\n${detalhesFrete}${total}\n${prazo}`;
+            });
+          } else if (transportadora.transp_nome !== "Retira") {
             const cardElement = document.createElement("div");
             cardElement.classList.add("card");
             const titulo = document.createElement("p");
@@ -435,7 +513,7 @@ Simulação de Frete
                   produtosDescricao += produtoDescricao;
                 }
               }
-              const titulo =  transportadora.transp_nome;
+              const titulo = transportadora.transp_nome;
               const frete = transportadora.vlrFrete;
               const prazoEntrega = transportadora.prazoEnt;
               let valorTotal = 0;
@@ -449,7 +527,7 @@ Simulação de Frete
               valorTotal += parseFloat(frete);
               const valorTotalFormatado = valorTotal.toFixed(2);
               const prazoConfeccao = prazoConfecaoMaisAlto;
-              const detalhesFrete = `Retirada na loja\n\n`;
+              const detalhesFrete = `Frete: ${cepDestino} - R$${frete} - (Dia da postagem + ${prazoEntrega} dias úteis via ${titulo})\n\n`;
               const total = `Total: R$${valorTotalFormatado}\n`;
               const prazo = `Prazo para confecção é de ${prazoConfeccao} dias úteis + prazo de envio.\nPrazo inicia-se após aprovação da arte e pagamento confirmado`;
               campoTexto.value = `${produtosDescricao}\n${detalhesFrete}${total}\n${prazo}`;
