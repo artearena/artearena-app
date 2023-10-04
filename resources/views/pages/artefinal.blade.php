17ff40f0958a3360b2be41a36f003d30b99ba25f
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-04 18:01:09 -0300

    att

--- resources/views/pages/artefinal.blade.php
+++ resources/views/pages/artefinal.blade.php
@@ -5,7 +5,7 @@ Consulta de Pedidos
 @endsection
 
 @section('style')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
+<!-- Carregue o jQuery primeiro -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.0/css/fixedHeader.dataTables.min.css">
