feba557bf1d7032deca4532930c60ca4b4bfdb41
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-06 16:21:02 -0300

    up

--- public/script_teste.php
+++ public/script_teste.php
@@ -12,7 +12,6 @@ for ($pagina = 1; $pagina <= 37; $pagina++) {
         "pesquisa" => "",
         "pagina" => $pagina
     ];
-
     $url_completa = $url . '?' . http_build_query($params);
     $response = file_get_contents($url_completa);
     $data = json_decode($response, true);
@@ -25,7 +24,6 @@ $servername = "mysql.arte.app.br";
 $username = "arte02";
 $password = "140477nagy";
 $dbname = "arte02";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Erro de conexão com o banco de dados: " . $conn->connect_error);
@@ -46,88 +44,8 @@ function produto_existe($conn, $produto_id)
 
 // Importar produtos no banco de dados
 $registros_inseridos = 0;
-$excecoes= [ 
-    "AGULHA", 
-    "Arruela ferro", 
-    "Borda Mastro", 
-    "Caixa de papelão (139,21)", 
-    "Caneta Esferográfica Azul", 
-    "Caneta Esferográfica Preta", 
-    "Cola Chinelo", 
-    "Cola Instantânea Bond 793 100g", 
-    "Cola tecido", 
-    "CORDÃO SACOCHILA", 
-    "CORDÃO SHORT", 
-    "Elástico para roupas 4cm", 
-    "Elástico para roupas 5cm", 
-    "Espuma densidade 45 - 1,4 x 0,95 - Cinza", 
-    "Fio - Branco", 
-    "Fio - Preto", 
-    "Fita Adesiva 12mm", 
-    "Fita Adesiva 45mm", 
-    "Fita Adesiva Sublimação - 5 mm x 33 m", 
-    "Fita Crepe 18mm", 
-    "Folha A4 Sulfite", 
-    "Franja 5mm Amarelo", 
-    "Franja 5mm Branco", 
-    "Franja 5mm Dourado", 
-    "Franja 5mm Preto", 
-    "Franja 5mm Vermelho", 
-    "Haste Bandeira de mesa de Madeira - Dupla", 
-    "Haste Bandeira de mesa de Madeira - Individual", 
-    "Haste Bandeira de mesa de Madeira - Tripla", 
-    "Haste bandeira de mesa de Plástico - Preto", 
-    "Ilhós metálico", 
-    "Kraft", 
-    "Linha - Branco", 
-    "Linha - Invisível", 
-    "Mosquetão", 
-    "Novobag (faixa de capitão)", 
-    "Nylon", 
-    "Papel Sublimático (Digiprint)", 
-    "Papel Sublimático (PLOTAG 300mts)", 
-    "Saco de lixo 100L", 
-    "Saco de Notas", 
-    "Saco Plástico 26x36", 
-    "Saco Plástico 32x40", 
-    "Saco Plástico 40x50", 
-    "Saco Plástico 50x60", 
-    "SUBLIMACAO INK JET PAPIJET AMARELO 1 Kg", 
-    "SUBLIMACAO INK JET PAPIJET CYAN 1 Kg", 
-    "SUBLIMACAO INK JET PAPIJET MAGENTA 1 Kg", 
-    "SUBLIMACAO INK JET PAPIJET PRETO 1 kg", 
-    "Suporte Bandeira Mesa (Madeira)", 
-    "Tecido - Bember", 
-    "Tecido - Malha PP", 
-    "Tecido Atoalhado", 
-    "Tecido Cetim", 
-    "Tecido Chimpa", 
-    "Tecido Dryfit Colmeia", 
-    "Tecido Helanca", 
-    "Tecido Oxford", 
-    "Tecido Tactel 2 cabos", 
-    "Tecido Tactel 4 cabos", 
-    "TESOURA", 
-    "TINTA IMPRESSORA (ADM) AMARELO", 
-    "TINTA IMPRESSORA (ADM) CIANO", 
-    "TINTA IMPRESSORA (ADM) MAGENTA", 
-    "TINTA IMPRESSORA (ADM) PRETO", 
-    "Veda Rosca", 
-    "Vedante", 
-    "Velcro", 
-    "Ziper - Branco", 
-    "Zíper - Preto", 
-    "Açúcar", 
-    "Álcool em Gel", 
-    "Café", 
-    "Detergente (500 ml)", 
-    "Esponja", 
-    "Papel Higiênico", 
-    "Papel Toalha", 
-    "Protetor Auricular", 
-    "Sabão em Pó", 
-    "Sabonete Líquido", 
-    "Veja (Produto de limpeza)" 
+$excecoes = [
+    // Lista de exceções de produtos
 ];
 
 foreach ($produtos_totais as $produto) {
@@ -143,7 +61,6 @@ foreach ($produtos_totais as $produto) {
             // Inserir
             $sql_insert = "INSERT INTO produtos (id, data_criacao, nome, codigo, preco, preco_promocional, unidade, gtin, tipoVariacao, localizacao, preco_custo, preco_custo_medio, situacao) ";
             $sql_insert .= "VALUES (?, STR_TO_DATE(?, '%d/%m/%Y %H:%i:%s'), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
             $stmt = $conn->prepare($sql_insert);
             $stmt->bind_param(
                 "ssssddssssdds",
@@ -161,50 +78,52 @@ foreach ($produtos_totais as $produto) {
                 $produto_info['preco_custo_medio'],
                 $produto_info['situacao']
             );
-
             if ($stmt->execute()) {
                 $registros_inseridos++;
             } else {
                 $excecoes[] = $stmt->error;
             }
-
             $stmt->close();
         }
-
-        // Exibir o progresso
-        echo $registros_inseridos . " registros inseridos...\n";
     }
 }
-foreach ($ids_produtos as $id) {
-    obterDadosProduto($conn, $token, $id);
-    echo $registros_inseridos . " registro " . $id . "atualizado...\n";
+
+foreach ($produtos_totais as $produto) {
+    $produto_info = $produto['produto'];
+    obterDadosProduto($conn, $token, $produto_info['id']);
+    echo $registros_inseridos . " registro " . $produto_info['id'] . " atualizado...\n";
 }
+
 // Função para obter os dados do produto e atualizar no banco de dados
-function obterDadosProduto($conn, $token, $id) {
+function obterDadosProduto($conn, $token, $id)
+{
     $url = "https://api.tiny.com.br/api2/produto.obter.php";
     $formato = "json";
-    
     $params = [
         "token" => $token,
         "id" => $id,
         "formato" => $formato
     ];
-    
     $url_completa = $url . '?' . http_build_query($params);
     $response = file_get_contents($url_completa);
     $data = json_decode($response, true);
-    
     if ($data['retorno']['status'] == "OK") {
         $produto = $data['retorno']['produto'];
-        
         $sql_update = "UPDATE produtos SET altura = ?, largura = ?, comprimento = ?, ncm = ?, anexo = ? WHERE id = ?";
         $stmt = $conn->prepare($sql_update);
-        $stmt->bind_param("dddsds", $produto['alturaEmbalagem'], $produto['larguraEmbalagem'], $produto['comprimentoEmbalagem'], $produto['ncm'], $produto['anexos'][0]['anexo'], $id);
+        $stmt->bind_param(
+            "dddsds",
+            $produto['alturaEmbalagem'],
+            $produto['larguraEmbalagem'],
+            $produto['comprimentoEmbalagem'],
+            $produto['ncm'],
+            $produto['anexos'][0]['anexo'],
+            $id
+        );
         $stmt->execute();
         $stmt->close();
     }
 }
 
 $conn->close();
-
 ?>
