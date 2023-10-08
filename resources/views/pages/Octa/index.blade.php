05082ae04238e22277f56cd0280dc292a8b03b24
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-08 10:36:21 -0300

    att

--- resources/views/pages/Octa/index.blade.php
+++ resources/views/pages/Octa/index.blade.php
@@ -44,54 +44,70 @@
         tbody tr:hover {
             background-color: #ebebeb;
         }
-        .switch {
 
-            width: 50px;
-            height: 20px;
-            text-align: center;
-            margin: -30px 0 0 -75px;
-            background: #00bc9c;
-            transition: all 0.2s ease;
-            border-radius: 25px;
-        }
-        .switch span {
-            position: absolute;
-            width: 20px;
-            height: 4px;
-            top: 50%;
-            left: 50%;
-            margin: -2px 0px 0px -4px;
-            background: #fff;
-            display: block;
-            transform: rotate(-45deg);
-            transition: all 0.2s ease;
-        }
-        .switch span:after {
-            content: "";
-            display: block;
-            position: absolute;
-            width: 4px;
-            height: 12px;
-            margin-top: -8px;
-            background: #fff;
-            transition: all 0.2s ease;
-        }
-        input[type=radio] {
-            display: none;
-        }
-        .switch label {
-            cursor: pointer;
-            color: rgba(0,0,0,0.2);
+        /* The switch - the box around the slider */
+        .switch {
+            position: relative;
+            display: inline-block;
             width: 60px;
-            line-height: 50px;
-            transition: all 0.2s ease;
+            height: 34px;
         }
-        label[for=yes] {
+
+        /* Hide default HTML checkbox */
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+
+        /* The slider */
+        .slider {
             position: absolute;
-            left: 0px;
-            height: 20px;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            -webkit-transition: .4s;
+            transition: .4s;
         }
-        
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 26px;
+            width: 26px;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+
+        input:checked + .slider {
+            background-color: #2196F3;
+        }
+
+        input:focus + .slider {
+            box-shadow: 0 0 1px #2196F3;
+        }
+
+        input:checked + .slider:before {
+            -webkit-transform: translateX(26px);
+            -ms-transform: translateX(26px);
+            transform: translateX(26px);
+        }
+
+        /* Rounded sliders */
+        .slider.round {
+            border-radius: 34px;
+        }
+
+        .slider.round:before {
+            border-radius: 50%;
+        }
+
     </style>
 @endsection
 @section('content')
@@ -178,15 +194,10 @@
                     </select>
                 </td>
                 <td class="text-center">
-                    <div class="toggle-radio">
-                        <input type="radio" name="rdo" id="yes" checked>
-                        <input type="radio" name="rdo" id="no">
-                        <div class="switch">
-                            <label for="yes">Sim</label>
-                            <label for="no">Não</label>
-                            <span></span>
-                        </div>
-                    </div>
+                <label class="switch">
+                    <input type="checkbox">
+                    <span class="slider round"></span>
+                </label>
                 </td>
                 <td class="text-center">Prov.</td>
                 <td class="text-center">Prov.</td>
