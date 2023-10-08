10a7d0afb65163fb2103389d760d6b7f4404bdfd
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-08 13:31:13 -0300

    att

--- resources/views/pages/Octa/index.blade.php
+++ resources/views/pages/Octa/index.blade.php
@@ -195,7 +195,7 @@
                 </td>
                 <td class="text-center">
                     <label class="switch">
-                        <input type="checkbox" class="table_checkbox" id="checkbox">
+                        <input type="checkbox" class="table_checkbox" id="checkbox" value="{{ cliente->contato_bloqueado }}">
                         <span class="slider round"></span>
                     </label>
                 </td>
