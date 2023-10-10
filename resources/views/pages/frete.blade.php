7c340d8f2cc36feaf1957d1f614be02c6e7da275
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-10 09:57:46 -0300

    teste

--- resources/views/pages/frete.blade.php
+++ resources/views/pages/frete.blade.php
@@ -142,7 +142,7 @@
     <div class="container mt-4">
         <h1>Gerar Orçamentos</h1>
         <hr>
-        <div class="row">
+        <div class="row justify-content-between">
             <div class="col-md-6">
                 <form id="opt-octa-form" method="POST" action="">
                     <div class="form-group">
@@ -231,7 +231,7 @@
                 </div>
                 <button type="button" class="btn btn-primary" id="calcularFrete">Calcular</button>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-3">
                 <div class="row">
                     <div class="col-sm-12">
                         <h4>Detalhes do orçamento:</h4>
