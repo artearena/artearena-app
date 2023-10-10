996ffa4303a9e46efbe6349d6afe78d63378f10f
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-10 17:22:04 -0300

    att

--- resources/views/pages/frete.blade.php
+++ resources/views/pages/frete.blade.php
@@ -1049,6 +1049,7 @@
               dataPrevistaFormatada = null;
               prazoEntrega = null;
             }
+            
           return {
             nomeTransportadora,
             valorFrete,
