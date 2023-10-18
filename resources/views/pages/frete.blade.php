61c13cf908e9ec14ce30d11b3c5476bc6a7e7d48
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-18 17:33:49 -0300

    atualizar jquery individual

--- resources/views/pages/frete.blade.php
+++ resources/views/pages/frete.blade.php
@@ -228,7 +228,8 @@
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 
 @endsection
 
