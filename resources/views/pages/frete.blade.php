b38ccad8f21cdcb50060554c429448a255e9bb50
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-18 16:52:54 -0300

    att

--- resources/views/pages/frete.blade.php
+++ resources/views/pages/frete.blade.php
@@ -228,8 +228,9 @@
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+
 
 @endsection
 
