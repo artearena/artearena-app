478f5aa2c2d5327c11e06537477024bf2dd622df
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-20 15:19:30 -0300

    att btn

--- resources/views/pages/frete.blade.php
+++ resources/views/pages/frete.blade.php
@@ -958,8 +958,10 @@ const id_cliente = document.getElementById('id').value;
                   <td>${orcamento.prazo_entrega}</td>
                   <td>${orcamento.data_prevista}</td>
                   <td>${orcamento.logo_frete}</td>
-                  <td><button class="btn btn-primary btn-carregar" onclick="carregarDados(this)">Carregar</button></td>
-                  <td><button class="btn btn-success btn-criar-pedido" onclick="criarPedido(${orcamento.id})">Criar Pedido</button></td>
+                  <td>
+                    <button class="btn btn-primary btn-carregar" onclick="carregarDados(this)">Carregar</button>
+                    <button class="btn btn-success btn-criar-pedido" onclick="criarPedido(${orcamento.id})">Cria Pedido</button>
+                  </td>
                 </tr>
               `;
             });
