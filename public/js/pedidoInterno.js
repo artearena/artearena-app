e1a90a4e67e93c0ff5e7b43e7a9f14b60f91440b
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-25 09:56:16 -0300

    att

--- public/js/pedidoInterno.js
+++ public/js/pedidoInterno.js
@@ -1,4 +1,4 @@
-console.log('to aqui');
+console.log('to aqui att');
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('btnCriarPedido').addEventListener('click', function(event) {
         event.preventDefault(); // Impede o comportamento padrão do botão
