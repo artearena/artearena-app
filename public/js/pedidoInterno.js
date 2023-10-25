2cfde0a5f3f5b2292ea4e116ef1e1b371463d9f4
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-25 10:28:25 -0300

    att csrf

--- public/js/pedidoInterno.js
+++ public/js/pedidoInterno.js
@@ -44,6 +44,9 @@ document.addEventListener("DOMContentLoaded", function() {
             type: "POST",
             data: JSON.stringify(pedido),
             contentType: "application/json",
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
             success: function(data) {
                 Swal.fire({
                     title: "Sucesso!",
