454793cd454ea0f75eddca575b8b34e4d91446c2
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-25 16:51:08 -0300

    att

--- resources/views/pages/pedidoInterno/index.blade.php
+++ resources/views/pages/pedidoInterno/index.blade.php
@@ -66,7 +66,7 @@
                 <td>{{ $pedido->forma_pagamento }}</td>
                 <td>{{ $pedido->transportadora }}</td>
                 <td>{{ $pedido->valor_frete }}</td>
-                <td id="observacao" style="overflow: auto;">{{ $pedido->observacao }}</td>
+                <td id="observacao" style="overflow: auto;" lang="pt">{{ $pedido->observacao }}</td>
                 <td>{{ $pedido->marcador }}</td>
                 <td>{{ $pedido->data_venda }}</td>
                 <td>
