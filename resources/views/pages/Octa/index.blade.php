2dc116b45287d149445ecfd9591843460d242dc9
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-27 11:49:20 -0300

    att teste

--- resources/views/pages/Octa/index.blade.php
+++ resources/views/pages/Octa/index.blade.php
@@ -140,7 +140,11 @@
             <tr>
                 <td class="cliente-id text-center" style="display:none">{{ $cliente->id }}</td>
                 <td class="text-center">{{ $cliente->id }}</td>
-
+                <td class="text-center" style="word-wrap: break-word;">
+                    <a href="https://app.octadesk.com/chat/{{ $cliente->url_octa }}/opened" target="_blank">
+                        <?php echo chunk_split($cliente->nome, 25, "<br>"); ?>
+                    </a>
+                </td>
                 <td class="text-center">{{ $cliente->telefone }}</td>
                 <td class="text-center" style="display:none">{{ $cliente->email }}</td>
                 <td class="text-center">{{ $cliente->empresa }}</td>
@@ -212,15 +216,6 @@
     </div>
 @endsection
 @section('extraScript')
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/pt-br.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
-    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
 
     <script>
         $(document).ready(function() {
