17cd6913c7f6b7b7b54868c6bbd147c4da9cbf49
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-30 16:04:04 -0300

    att

--- app/Http/Middleware/ValidarToken.php
+++ app/Http/Middleware/ValidarToken.php
@@ -1,21 +1,17 @@
 <?php
-
 namespace App\Http\Middleware;
-
 use Closure;
 use Illuminate\Support\Facades\DB;
-
 class ValidarToken
 {
     public function handle($request, Closure $next)
     {
         $token = $request->query('token');
         $acessoTemporario = DB::table('acesso_temporario')->where('token', $token)->first();
-
         if (!$acessoTemporario || $acessoTemporario->validade < now()) {
             return back()->withErrors(['token' => 'Token inválido ou expirado.']);
         }
-
+        dd($acessoTemporario); // Adicionando dd() para depuração
         return $next($request);
     }
 }
