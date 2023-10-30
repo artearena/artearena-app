57264c0a10efb7bb2532ecfa8dc696deb72ad80e
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-30 13:29:21 -0300

    uniformes

--- resources/views/pages/listaUniformes/index.blade.php
+++ resources/views/pages/listaUniformes/index.blade.php
@@ -1,72 +1,98 @@
 @extends('layout.main')
-@section('title', 'Cadastro pessoal')
+@section('title', 'Lista de Uniformes')
 @section('content')
     <div class="container">
-        <h1>Cadastro pessoal</h1>
+        <h1>Lista de Uniformes</h1>
         <form id="cadastroForm">
+            <div class="form-group">
+                <label for="pacote">Pacote:</label>
+                <select class="form-control" id="pacote" name="pacote">
+                    <option value="start">Start</option>
+                    <option value="prata">Prata</option>
+                    <option value="ouro">Ouro</option>
+                    <option value="diamante">Diamante</option>
+                    <option value="premium">Premium</option>
+                    <option value="profissional">Profissional</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Uniformes:</label><br>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="camisa" name="uniforme" value="camisa">
+                    <label class="form-check-label" for="camisa">Camisa</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="calcao" name="uniforme" value="calcao">
+                    <label class="form-check-label" for="calcao">Calção</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="meiao" name="uniforme" value="meiao">
+                    <label class="form-check-label" for="meiao">Meião</label>
+                </div>
+            </div>
             <table id="registrosTable" class="table mt-4">
                 <thead>
                     <tr>
-                        <th>Produto</th>
+                        <th>Uniforme</th>
                         <th>Quantidade</th>
+                        <th>Tamanho</th>
                     </tr>
                 </thead>
                 <tbody id="registrosTableBody">
                 </tbody>
             </table>
             <div class="form-group">
-                <label for="produto">Produto:</label>
-                <input type="text" class="form-control" id="produto" name="produto">
+                <label for="uniforme">Uniforme:</label>
+                <input type="text" class="form-control" id="uniforme" name="uniforme">
             </div>
             <div class="form-group">
                 <label for="quantidade">Quantidade:</label>
                 <input type="text" class="form-control" id="quantidade" name="quantidade">
+            </div>
+            <div class="form-group">
+                <label for="tamanho">Tamanho:</label>
+                <input type="text" class="form-control" id="tamanho" name="tamanho">
             </div>
             <button type="button" onclick="adicionarRegistro()" class="btn btn-primary">Adicionar Registro</button>
         </form>
     </div>
     <script>
         var registros = [];
-
         function adicionarRegistro() {
-            var produto = document.getElementById('produto').value;
+            var uniforme = document.getElementById('uniforme').value;
             var quantidade = document.getElementById('quantidade').value;
-
+            var tamanho = document.getElementById('tamanho').value;
             var registro = {
-                produto: produto,
-                quantidade: quantidade
+                uniforme: uniforme,
+                quantidade: quantidade,
+                tamanho: tamanho
             };
-
             registros.push(registro);
-
             atualizarTabela();
             limparCampos();
         }
-
         function atualizarTabela() {
             var tabela = document.getElementById('registrosTableBody');
             tabela.innerHTML = '';
-
             for (var i = 0; i < registros.length; i++) {
                 var registro = registros[i];
-
                 var tr = document.createElement('tr');
-
-                var tdProduto = document.createElement('td');
-                tdProduto.innerHTML = '<input type="text" class="form-control" value="' + registro.produto + '">';
-                tr.appendChild(tdProduto);
-
+                var tdUniforme = document.createElement('td');
+                tdUniforme.innerHTML = '<input type="text" class="form-control" value="' + registro.uniforme + '">';
+                tr.appendChild(tdUniforme);
                 var tdQuantidade = document.createElement('td');
                 tdQuantidade.innerHTML = '<input type="text" class="form-control" value="' + registro.quantidade + '">';
                 tr.appendChild(tdQuantidade);
-
+                var tdTamanho = document.createElement('td');
+                tdTamanho.innerHTML = '<input type="text" class="form-control" value="' + registro.tamanho + '">';
+                tr.appendChild(tdTamanho);
                 tabela.appendChild(tr);
             }
         }
-
         function limparCampos() {
-            document.getElementById('produto').value = '';
+            document.getElementById('uniforme').value = '';
             document.getElementById('quantidade').value = '';
+            document.getElementById('tamanho').value = '';
         }
     </script>
 @endsection
