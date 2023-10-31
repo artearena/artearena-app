b02307dd8b0c6dfc2be914156c5f329750312cc8
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-10-31 10:43:15 -0300

    att

--- resources/views/pages/pedidoInterno/index.blade.php
+++ resources/views/pages/pedidoInterno/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.main')
+
 @section('title', 'Tabela de Pedidos')
+
 @section('content')
     <div class="container">
         <h1>Tabela de Pedidos</h1>
@@ -21,44 +23,44 @@
             </thead>
             <tbody>
                 @isset($pedidos)
-                @foreach($pedidos as $pedido)
-                <tr class="pedido-row" data-pedido-id="{{ $pedido->id }}">
-                    <td>{{ $pedido->id }}</td>
-                    <td>{{ $pedido->cliente_id }}</td>
-                    <td>{{ $pedido->Vendedor }}</td>
-                    <td>
-                        <button class="btn btn-link btn-expand-produtos">Expandir</button>
-                    </td>
-                    <td>{{ $pedido->forma_pagamento }}</td>
-                    <td>{{ $pedido->transportadora }}</td>
-                    <td>{{ $pedido->valor_frete }}</td>
-                    <td id="observacao" style="overflow: auto;" lang="pt">{{ $pedido->observacao }}</td>
-                    <td>{{ $pedido->marcador }}</td>
-                    <td>{{ $pedido->data_venda }}</td>
-                    <td>
-                        <div class="btn-group">
-                            <button class="btn btn-success btn-confirmar-pedido">
-                                <i class="fas fa-check"></i>
-                            </button>
-                            <button class="btn btn-primary btn-consultar-lista-uniforme" data-toggle="modal" data-target="#modalListaUniforme" data-pedido-id="{{ $pedido->id }}">
-                                <i class="fas fa-tshirt"></i>
-                            </button>
-                            <button class="btn btn-warning btn-salvar-consultar-cliente" data-cliente-id="{{ $pedido->cliente_id }}">
-                                <i class="fas fa-link"></i>
-                            </button>
-                        </div>
-                    </td>
-                </tr>
-                <tr class="produtos-row" style="display: none;">
-                    <td colspan="11">
-                        <ul>
-                            @foreach($pedido->produtos ?? [] as $produto)
-                                <li>{{ $produto->produto_nome }} (Quantidade: {{ $produto->quantidade }}, Preço Unitário: {{ $produto->preco_unitario }})</li>
-                            @endforeach
-                        </ul>
-                    </td>
-                </tr>
-                @endforeach
+                    @foreach($pedidos as $pedido)
+                        <tr class="pedido-row" data-pedido-id="{{ $pedido->id }}">
+                            <td>{{ $pedido->id }}</td>
+                            <td>{{ $pedido->cliente_id }}</td>
+                            <td>{{ $pedido->Vendedor }}</td>
+                            <td>
+                                <button class="btn btn-link btn-expand-produtos">Expandir</button>
+                            </td>
+                            <td>{{ $pedido->forma_pagamento }}</td>
+                            <td>{{ $pedido->transportadora }}</td>
+                            <td>{{ $pedido->valor_frete }}</td>
+                            <td id="observacao" style="overflow: auto;" lang="pt">{{ $pedido->observacao }}</td>
+                            <td>{{ $pedido->marcador }}</td>
+                            <td>{{ $pedido->data_venda }}</td>
+                            <td>
+                                <div class="btn-group">
+                                    <button class="btn btn-success btn-confirmar-pedido">
+                                        <i class="fas fa-check"></i>
+                                    </button>
+                                    <button class="btn btn-primary btn-consultar-lista-uniforme" data-toggle="modal" data-target="#modalListaUniforme" data-pedido-id="{{ $pedido->id }}">
+                                        <i class="fas fa-tshirt"></i>
+                                    </button>
+                                    <button class="btn btn-warning btn-salvar-consultar-cliente" data-cliente-id="{{ $pedido->cliente_id }}">
+                                        <i class="fas fa-link"></i>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="produtos-row" style="display: none;">
+                            <td colspan="11">
+                                <ul>
+                                    @foreach($pedido->produtos ?? [] as $produto)
+                                        <li>{{ $produto->produto_nome }} (Quantidade: {{ $produto->quantidade }}, Preço Unitário: {{ $produto->preco_unitario }})</li>
+                                    @endforeach
+                                </ul>
+                            </td>
+                        </tr>
+                    @endforeach
                 @endisset
             </tbody>
         </table>
@@ -72,7 +74,7 @@
                     fetch('/gerarLinkCadastroCliente?pedidoId=' + pedidoId)
                         .then(response => response.json())
                         .then(data => {
-                            alert('link temporario: ' + data.link);
+                            alert('link temporario:     ' + data.link);
                         });
                 });
             }
