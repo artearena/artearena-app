17c26c934588585b124df3c5ba900ded746d1b6e
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-03 13:39:11 -0300

    att

--- resources/views/pages/Octa/index.blade.php
+++ resources/views/pages/Octa/index.blade.php
@@ -163,111 +163,115 @@
                 }
             });
             function carregarRegistros() {
-        // Realizar a carga dos registros aqui
-        $.ajax({
-            url: '/crm/getDados/',
-            type: 'GET',
-            success: function(response) {
-                // Atualizar a tabela com os registros obtidos
-                var clientes = response.clientes;
-
-                // Limpar a tabela
-                $('#clientesTable tbody').empty();
-
-                // Iterar sobre os registros e adicionar na tabela
-                $.each(clientes, function(index, cliente) {
-                    var row = '<tr>' +
-                        '<td class="cliente-id text-center" style="display:none">' + cliente.id + '</td>' +
-                        '<td class="text-center">' + cliente.id + '</td>' +
-                        '<td class="text-center" style="word-wrap: break-word;">' +
-                        '<a href="https://app.octadesk.com/chat/' + cliente.url_octa + '/opened" target="_blank">' +
-                        chunkSplit(cliente.nome, 25, "<br>") +
-                        '</a>' +
-                        '</td>' +
-                        '<td class="text-center">' + cliente.telefone + '</td>' +
-                        '<td class="text-center" style="display:none">' + cliente.email + '</td>' +
-                        '<td class="text-center">' + cliente.empresa + '</td>' +
-                        '<td class="text-center">' +
-                        '<select name="responsavel_contato" class="form-control responsavel-contato">' +
-                        '<option value="">Selecione um responsável</option>';
-
-                    // Iterar sobre os vendedores e adicionar as opções do select
-                    $.each(response.vendedores, function(index, vendedor) {
-                        row += '<option value="' + vendedor + '"' +
-                            (cliente.responsavel_contato == vendedor ? ' selected' : '') +
-                            '>' + vendedor + '</option>';
-                    });
-
-                    row += '</select>' +
-                        '</td>' +
-                        '<td class="text-center" style="display:none">' + cliente.origem + '</td>' +
-                        '<td>' +
-                        '<select class="form-control" name="status_conversa">' +
-                        '<option value="">Selecione uma opção</option>' +
-                        '<option value="Lead"' + (cliente.status_conversa == 'Lead' ? ' selected' : '') + '>Lead</option>' +
-                        '<option value="Venda Concluída"' + (cliente.status_conversa == 'Venda Concluída' ? ' selected' : '') + '>Venda Concluída</option>' +
-                        '<option value="Enviado"' + (cliente.status_conversa == 'Enviado' ? ' selected' : '') + '>Enviado</option>' +
-                        '<option value="Aberto"' + (cliente.status_conversa == 'Aberto' ? ' selected' : '') + '>Aberto</option>' +
-                        '</select>' +
-                        '</td>' +
-                        '<td class="text-center">' + cliente.created_at + '</td>' +
-                        '<td class="text-center">' +
-                        '<div class="date datetimepicker">' +
-                        '<input type="datetime-local" class="form-control" id="date" lang="pt-br" ' +
-                        'value="' + (cliente.data_agendamento ? (new DateTime(cliente.data_agendamento)).format("Y-m-d\\TH:i:s") : '') + '">' +
-                        '<span class="input-group-addon">' +
-                        '<span class="glyphicon glyphicon-calendar"></span>' +
-                        '</span>' +
-                        '</div>' +
-                        '</td>' +
-                        '<td class="text-center">' +
-                        '<select name="mensagem_id" class="form-control mensagem_id" ' + (cliente.data_agendamento ? '' : 'disabled') + '>' +
-                        '<option value="">Selecione uma mensagem</option>';
-
-                    // Iterar sobre as mensagens e adicionar as opções do select
-                    $.each(response.mensagens, function(index, mensagem) {
-                        row += '<option value="' + mensagem.id + '"' +
-                            (cliente.mensagem_template_id == mensagem.id ? ' selected' : '') +
-                            '>' + mensagem.titulo + '</option>';
-                    });
-
-                    row += '</select>' +
-                        '</td>' +
-                        '<td class="text-center">' +
-                        '<label class="switch">' +
-                        '<input type="checkbox" class="table_checkbox" id="checkbox" value="' + cliente.contato_bloqueado + '"' +
-                        (cliente.contato_bloqueado == 1 ? ' checked' : '') +
-                        '>' +
-                        '<span class="slider round"></span>' +
-                        '</label>' +
-                        '</td>' +
-                        '<td class="text-center">Prov.</td>' +
-                        '<td class="text-center">Prov.</td>' +
-                        '<td class="text-center">Prov.</td>' +
-                        '<td class="text-center">Prov.</td>' +
-                        '<td class="text-center">' +
-                        '<a href="#" class="btn btn-primary ms-1" target="_blank">' +
-                        '<i class="fa-brands fa-trello"></i>' +
-                        '</a>' +
-                        '</td>' +
-                        '</tr>';
-
-                    // Adicionar a linha na tabela
-                    $('#clientesTable tbody').append(row);
+                // Realizar a carga dos registros aqui
+                $.ajax({
+                    url: '/crm/getDados/',
+                    type: 'GET',
+                    success: function(response) {
+                        // Atualizar a tabela com os registros obtidos
+                        var clientes = response.clientes;
+                        // Limpar a tabela
+                        $('#clientesTable tbody').empty();
+                        // Iterar sobre os registros e adicionar na tabela
+                        $.each(clientes, function(index, cliente) {
+                            var row = '<tr>' +
+                                '<td class="cliente-id text-center" style="display:none">' + cliente.id + '</td>' +
+                                '<td class="text-center">' + cliente.id + '</td>' +
+                                '<td class="text-center" style="word-wrap: break-word;">' +
+                                '<a href="https://app.octadesk.com/chat/' + cliente.url_octa + '/opened" target="_blank">' +
+                                wrapText(cliente.nome, 25) +
+                                '</a>' +
+                                '</td>' +
+                                '<td class="text-center">' + cliente.telefone + '</td>' +
+                                '<td class="text-center" style="display:none">' + cliente.email + '</td>' +
+                                '<td class="text-center">' + cliente.empresa + '</td>' +
+                                '<td class="text-center">' +
+                                '<select name="responsavel_contato" class="form-control responsavel-contato">' +
+                                '<option value="">Selecione um responsável</option>';
+                            // Iterar sobre os vendedores e adicionar as opções do select
+                            $.each(response.vendedores, function(index, vendedor) {
+                                row += '<option value="' + vendedor + '"' +
+                                    (cliente.responsavel_contato == vendedor ? ' selected' : '') +
+                                    '>' + vendedor + '</option>';
+                            });
+                            row += '</select>' +
+                                '</td>' +
+                                '<td class="text-center" style="display:none">' + cliente.origem + '</td>' +
+                                '<td>' +
+                                '<select class="form-control" name="status_conversa">' +
+                                '<option value="">Selecione uma opção</option>' +
+                                '<option value="Lead"' + (cliente.status_conversa == 'Lead' ? ' selected' : '') + '>Lead</option>' +
+                                '<option value="Venda Concluída"' + (cliente.status_conversa == 'Venda Concluída' ? ' selected' : '') + '>Venda Concluída</option>' +
+                                '<option value="Enviado"' + (cliente.status_conversa == 'Enviado' ? ' selected' : '') + '>Enviado</option>' +
+                                '<option value="Aberto"' + (cliente.status_conversa == 'Aberto' ? ' selected' : '') + '>Aberto</option>' +
+                                '</select>' +
+                                '</td>' +
+                                '<td class="text-center">' + cliente.created_at + '</td>' +
+                                '<td class="text-center">' +
+                                '<div class="date datetimepicker">' +
+                                '<input type="datetime-local" class="form-control" id="date" lang="pt-br" ' +
+                                'value="' + (cliente.data_agendamento ? (new DateTime(cliente.data_agendamento)).format("Y-m-d\\TH:i:s") : '') + '">' +
+                                '<span class="input-group-addon">' +
+                                '<span class="glyphicon glyphicon-calendar"></span>' +
+                                '</span>' +
+                                '</div>' +
+                                '</td>' +
+                                '<td class="text-center">' +
+                                '<select name="mensagem_id" class="form-control mensagem_id" ' + (cliente.data_agendamento ? '' : 'disabled') + '>' +
+                                '<option value="">Selecione uma mensagem</option>';
+                            // Iterar sobre as mensagens e adicionar as opções do select
+                            $.each(response.mensagens, function(index, mensagem) {
+                                row += '<option value="' + mensagem.id + '"' +
+                                    (cliente.mensagem_template_id == mensagem.id ? ' selected' : '') +
+                                    '>' + mensagem.titulo + '</option>';
+                            });
+                            row += '</select>' +
+                                '</td>' +
+                                '<td class="text-center">' +
+                                '<label class="switch">' +
+                                '<input type="checkbox" class="table_checkbox" id="checkbox" value="' + cliente.contato_bloqueado + '"' +
+                                (cliente.contato_bloqueado == 1 ? ' checked' : '') +
+                                '>' +
+                                '<span class="slider round"></span>' +
+                                '</label>' +
+                                '</td>' +
+                                '<td class="text-center">Prov.</td>' +
+                                '<td class="text-center">Prov.</td>' +
+                                '<td class="text-center">Prov.</td>' +
+                                '<td class="text-center">Prov.</td>' +
+                                '<td class="text-center">' +
+                                '<a href="#" class="btn btn-primary ms-1" target="_blank">' +
+                                '<i class="fa-brands fa-trello"></i>' +
+                                '</a>' +
+                                '</td>' +
+                                '</tr>';
+                            // Adicionar a linha na tabela
+                            $('#clientesTable tbody').append(row);
+                        });
+                    },
+                    error: function(xhr, status, error) {
+                        // Tratar erros, se necessário
+                    }
                 });
-            },
-            error: function(xhr, status, error) {
-                // Tratar erros, se necessário
             }
-        });
-}
-function chunkSplit(str, length, separator) {
-    var result = '';
-    for (var i = 0; i < str.length; i += length) {
-        result += str.substr(i, length) + separator;
+function wrapText(str, maxLength) {
+    var wrappedText = '';
+    var currentLine = '';
+
+    for (var i = 0; i < str.length; i++) {
+        currentLine += str.charAt(i);
+        if (currentLine.length >= maxLength) {
+            wrappedText += currentLine + "<br>";
+            currentLine = '';
+        }
     }
-    return result;
-}            
+
+    if (currentLine.length > 0) {
+        wrappedText += currentLine;
+    }
+
+    return wrappedText;
+}        
 $('.datetimepicker').on('change', function() {
                 var id = $(this).closest('tr').find('.cliente-id').text();
                 var newDateTime = $(this).closest('tr').find('#date').val();
