831f9fb601b24095f679c6046528e66bbb73cf58
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-06 15:52:43 -0300

    att

--- resources/views/pages/partials/cliente-row.blade.php
+++ resources/views/pages/partials/cliente-row.blade.php
@@ -70,6 +70,85 @@
     </tr>
 @endforeach
 <script>
+    $('.datetimepicker').on('change', function() {
+        var id = $(this).closest('tr').find('.cliente-id').text();
+        var newDateTime = $(this).closest('tr').find('#date').val();
+        console.log(newDateTime);
+
+        // Habilitar ou desabilitar o campo template_message com base no valor do campo data_agendamento
+        var templateMessageField = $(this).closest('tr').find('.mensagem_id');
+        if (newDateTime) {
+            templateMessageField.prop('disabled', false);
+        } else {
+            templateMessageField.prop('disabled', true);
+        }
+
+        // Enviar requisição AJAX para atualizar a tabela com os novos dados
+        $.ajax({
+            url: '/crm/atualizar-data/' + id,
+            method: 'PUT',
+            data: {
+                id: id,
+                newDateTime: newDateTime,
+                "_token": "{{ csrf_token() }}"
+            },
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            }
+        });
+    });
+    
+    $('.mensagem_id').on('change', function() {
+        var mensagemId = $(this).val();
+        var clienteId = $(this).closest('tr').find('.cliente-id').text();
+
+        // Enviar solicitação AJAX para atualizar o registro no banco de dados
+        $.ajax({
+            url: '/crm/atualizar-mensagem',
+            method: 'POST',
+            data: {
+                clienteId: clienteId,
+                mensagemId: mensagemId,
+                "_token": "{{ csrf_token() }}"
+
+            },
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            }
+        });
+    });
+
+    $('.responsavel-contato').on('change', function() {
+        var clienteId = $(this).closest('tr').find('.cliente-id').text();
+        var novoVendedor = $(this).val();
+
+        // Enviar requisição ao servidor para atualizar o vendedor
+        fetch(`/crm/atualizar-vendedor/${clienteId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': '{{ csrf_token() }}',
+            },
+            body: JSON.stringify({ responsavel_contato: novoVendedor }),
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log('Vendedor atualizado com sucesso!');
+            } else {
+                console.error('Falha ao atualizar o vendedor.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+        });
+    });
+
     $('.table_checkbox').on('change', function() {
         console.log('executado');
         var clienteId = $(this).closest('tr').find('.cliente-id').text();
@@ -91,4 +170,36 @@
             }
         });
     });
+</script>
+<script>
+    var smallBreak = 800; // Your small screen breakpoint in pixels
+    var columns = $('.dataTable tr').length;
+    var rows = $('.dataTable th').length;
+
+    $(document).ready(shapeTable());
+    $(window).resize(function() {
+        shapeTable();
+    });
+
+    function shapeTable() {
+        if ($(window).width() < smallBreak) {
+            for (i=0;i < rows; i++) {
+                var maxHeight = $('.dataTable th:nth-child(' + i + ')').outerHeight();
+                for (j=0; j < columns; j++) {
+                    if ($('.dataTable tr:nth-child(' + j + ') td:nth-child(' + i + ')').outerHeight() > maxHeight) {
+                        maxHeight = $('.dataTable tr:nth-child(' + j + ') td:nth-child(' + i + ')').outerHeight();
+                    }
+                    if ($('.dataTable tr:nth-child(' + j + ') td:nth-child(' + i + ')').prop('scrollHeight') > $('.dataTable tr:nth-child(' + j + ') td:nth-child(' + i + ')').outerHeight()) {
+                        maxHeight = $('.dataTable tr:nth-child(' + j + ') td:nth-child(' + i + ')').prop('scrollHeight');
+                    }
+                }
+                for (j=0; j < columns; j++) {
+                    $('.dataTable tr:nth-child(' + j + ') td:nth-child(' + i + ')').css('height',maxHeight);
+                    $('.dataTable th:nth-child(' + i + ')').css('height',maxHeight);
+                }
+            }
+        } else {
+            $('.dataTable td, .dataTable th').removeAttr('style');
+        }
+    }
 </script>
