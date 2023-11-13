a18e340f6cbead8616091013d3136d8b123fa5f9
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-13 17:18:19 -0300

    att

--- resources/views/pages/pedidoInterno/index.blade.php
+++ resources/views/pages/pedidoInterno/index.blade.php
@@ -109,7 +109,7 @@
                                     <button class="btn btn-success btn-confirmar-pedido">
                                         <i class="fas fa-check"></i>
                                     </button>
-                                    @if (is_string($pedido->produto_nome) && str_contains($pedido->produto_nome, ['Uniforme(s)', 'Camiseta', 'Camisa', 'Short', 'Shorts', 'Abadá']))
+                                    @if (is_string($pedido->produto_nome) && str_contains($pedido->produto_nome, ['Uniforme', 'Camiseta', 'Camisa', 'Short', 'Shorts', 'Abadá']))
                                         <button class="btn btn-primary btn-consultar-lista-uniforme" data-toggle="modal" data-target="#modalListaUniforme" data-pedido-id="{{ $pedido->id }}">
                                             <i class="fas fa-tshirt"></i>
                                         </button>
