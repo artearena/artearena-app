038b256d08b1b4b8d9736b28a52e211533e6fbfe
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-13 13:19:12 -0300

    tt

--- public/js/pedidoInterno.js
+++ public/js/pedidoInterno.js
@@ -12,7 +12,7 @@ botoesExpandir.forEach(function(botaoExpandir) {
     // Verifique se a linha de produtos já está visível
     var produtosRow = document.querySelector('.produto-pedido-' + pedidoId);
     console.log(produtosRow)
-    if (produtosRow.style.display != 'none') {
+    if (produtosRow != null) {
       console.log(pedidoId);
       // Se já estiver visível, oculte a linha de produtos
       document.querySelector('.produto-pedido-'+ pedidoId).style.display = 'none'; 
