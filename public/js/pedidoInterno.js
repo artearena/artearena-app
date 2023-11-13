f93d70aff209d511fd305a8a1bdcd8f82f0e9f0f
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-13 10:36:17 -0300

    att

--- public/js/pedidoInterno.js
+++ public/js/pedidoInterno.js
@@ -1,45 +1,41 @@
 console.log('teste-debug');
-
-
-
 // Selecione o botão "Expandir"
-var botaoExpandir = document.querySelector('.btn-expand-produtos');
-
-// Adicione o evento de clique ao botão
-botaoExpandir.addEventListener('click', function() {
-  // Obtenha o ID do pedido
-  var pedidoId = this.getAttribute('data-pedido-id');
-
-// Faça a requisição AJAX para obter a lista de produtos
-fetch('/crm/get-produtos-pedido/' + pedidoId)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(produtos) {
-    // Selecione a tabela onde você deseja adicionar as linhas dos produtos
-    var tabelaProdutos = document.getElementById('tabela-produtos');
-
-    // Verifique se é um único produto ou uma lista de produtos
-    if (Array.isArray(produtos)) {
-      // Caso seja uma lista de produtos
-      produtos.forEach(function(produto) {
-        var novaLinha = tabelaProdutos.insertRow();
-        var novaCelula = novaLinha.insertCell();
-        novaCelula.textContent = produto.produto_nome;
+var botoesExpandir = document.querySelectorAll('.btn-expand-produtos');
+// Adicione o evento de clique a todos os botões
+botoesExpandir.forEach(function(botaoExpandir) {
+  botaoExpandir.addEventListener('click', function() {
+    // Obtenha o ID do pedido
+    var pedidoId = this.closest('.pedido-row').getAttribute('data-pedido-id');
+    // Faça a requisição AJAX para obter a lista de produtos
+    fetch('/crm/get-produtos-pedido/' + pedidoId)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(produtos) {
+        // Selecione a tabela onde você deseja adicionar as linhas dos produtos
+        var tabelaProdutos = document.getElementById('tabela-produtos');
+        // Limpe a tabela de produtos antes de adicionar novas linhas
+        tabelaProdutos.innerHTML = '';
+        // Verifique se é um único produto ou uma lista de produtos
+        if (Array.isArray(produtos)) {
+          // Caso seja uma lista de produtos
+          produtos.forEach(function(produto) {
+            var novaLinha = tabelaProdutos.insertRow();
+            var novaCelula = novaLinha.insertCell();
+            novaCelula.textContent = produto.produto_nome;
+          });
+        } else {
+          // Caso seja um único produto
+          var novaLinha = tabelaProdutos.insertRow();
+          var novaCelula = novaLinha.insertCell();
+          novaCelula.textContent = produtos.produto_nome;
+        }
+      })
+      .catch(function(error) {
+        console.log('Ocorreu um erro:', error);
       });
-    } else {
-      // Caso seja um único produto
-      var novaLinha = tabelaProdutos.insertRow();
-      var novaCelula = novaLinha.insertCell();
-      novaCelula.textContent = produtos.produto_nome;
-    }
-  })
-  .catch(function(error) {
-    console.log('Ocorreu um erro:', error);
   });
 });
-
-// Separador // Separador // Separador // Separador // Separador // Separador // Separador // Separador // Separador // Separador // Separador // Separador // Separador 
 
 document.addEventListener("DOMContentLoaded", function() {
   $(".btn-confirmar-pedido").click(function() {
@@ -61,33 +57,26 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Estilização da tabela, não mexer
-const smallBreak = 800; 
-
+const smallBreak = 800;
 $(document).ready(shapeTable);
 $(window).resize(shapeTable);
-
 function shapeTable() {
   if ($(window).width() < smallBreak) {
     const rows = $('.dataTable tr').length;
     const columns = $('.dataTable th').length;
-
     for (let i = 0; i < columns; i++) {
       let maxHeight = $('.dataTable th:nth-child(' + (i + 1) + ')').outerHeight();
-
       for (let j = 0; j < rows; j++) {
         const td = $('.dataTable tr:nth-child(' + (j + 1) + ') td:nth-child(' + (i + 1) + ')');
         const tdHeight = td.outerHeight();
         const tdScrollHeight = td.prop('scrollHeight');
-
         if (tdHeight > maxHeight) {
           maxHeight = tdHeight;
         }
-
         if (tdScrollHeight > tdHeight) {
           maxHeight = tdScrollHeight;
         }
       }
-
       $('.dataTable th:nth-child(' + (i + 1) + ')').css('height', maxHeight);
       $('.dataTable td:nth-child(' + (i + 1) + ')').css('height', maxHeight);
     }
