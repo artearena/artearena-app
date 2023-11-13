2170b942d40ad7858cc07b35ce95f606f97b8ebc
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-13 17:47:44 -0300

    att

--- resources/views/pages/pedidoInterno/index.blade.php
+++ resources/views/pages/pedidoInterno/index.blade.php
@@ -88,7 +88,7 @@
             </thead>
             <tbody>
                 @isset($pedidos)
-                @foreach($pedidos as $pedido)
+                @foreach ($listaProdutos as $listaProduto)
                     <tr class="pedido-row" data-pedido-id="{{ $pedido->id }}">
                         <td>{{ $pedido->id }}</td>
                         <td>{{ $pedido->cliente_id }}</td>
@@ -98,9 +98,39 @@
                                 Expandir
                             </button>
                         </td>                            
-                        <td>{{ $pedido->forma_pagamento }}</td>
-                        <td>{{ $pedido->transportadora }}</td>
-                        <td>{{ $pedido->valor_frete }}</td>
+                        <td>
+                            <select name="sexo" class="form-control">
+                                <option value="M" {{ $listaProduto->sexo == 'M' ? 'selected' : '' }}>M</option>
+                                <option value="F" {{ $listaProduto->sexo == 'F' ? 'selected' : '' }}>F</option>
+                            </select>
+                        </td>
+                        <td>
+                            <select name="pacote" class="form-control">
+                                <option value="start" {{ $listaProduto->pacote == 'start' ? 'selected' : '' }}>Start</option>
+                                <option value="prata" {{ $listaProduto->pacote == 'prata' ? 'selected' : '' }}>Prata</option>
+                                <option value="ouro" {{ $listaProduto->pacote == 'ouro' ? 'selected' : '' }}>Ouro</option>
+                                <option value="diamante" {{ $listaProduto->pacote == 'diamante' ? 'selected' : '' }}>Diamante</option>
+                                <option value="premium" {{ $listaProduto->pacote == 'premium' ? 'selected' : '' }}>Premium</option>
+                                <option value="profissional" {{ $listaProduto->pacote == 'profissional' ? 'selected' : '' }}>Profissional</option>
+                            </select>
+                        </td>
+                        <td>
+                            <select name="lista_aprovada" class="form-control">
+                                <option value="sim" {{ $listaProduto->lista_aprovada == 'sim' ? 'selected' : '' }}>Sim</option>
+                                <option value="não" {{ $listaProduto->lista_aprovada == 'não' ? 'selected' : '' }}>Não</option>
+                            </select>
+                        </td>
+                        <td>
+                            <select name="tamanho" class="form-control">
+                                <option value="P" {{ $listaProduto->tamanho == 'P' ? 'selected' : '' }}>P</option>
+                                <option value="M" {{ $listaProduto->tamanho == 'M' ? 'selected' : '' }}>M</option>
+                                <option value="G" {{ $listaProduto->tamanho == 'G' ? 'selected' : '' }}>G</option>
+                                <option value="GG" {{ $listaProduto->tamanho == 'GG' ? 'selected' : '' }}>GG</option>
+                                <option value="XG" {{ $listaProduto->tamanho == 'XG' ? 'selected' : '' }}>XG</option>
+                                <option value="XGG" {{ $listaProduto->tamanho == 'XGG' ? 'selected' : '' }}>XGG</option>
+                                <option value="XGGG" {{ $listaProduto->tamanho == 'XGGG' ? 'selected' : '' }}>XGGG</option>
+                            </select>
+                        </td>
                         <td id="observacao" style="overflow: auto;" lang="pt">{{ $pedido->observacao }}</td>
                         <td>{{ $pedido->marcador }}</td>
                         <td>{{ $pedido->data_venda }}</td>
@@ -134,6 +164,7 @@
                         </td>
                     </tr>
                 @endforeach
+                
                 @endisset
             </tbody>
         </table>
