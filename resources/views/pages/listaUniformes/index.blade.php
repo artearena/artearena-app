04b454e6493f835595844016360aa7e9127312d3
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-16 16:33:26 -0300

    att

--- resources/views/pages/listaUniformes/index.blade.php
+++ resources/views/pages/listaUniformes/index.blade.php
@@ -1,8 +1,39 @@
 @extends('layout.main')
 
-@section('title', 'Lista de informações dos produtos')
+@section('title', 'Detalhes do Produto')
 
 @section('content')
 
+    <div class="container">
+
+        <h1>Detalhes do Produto</h1>
+
+        <div class="divProduto">
+
+            <table class="table">
+
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Quantidade</th>
+                        <th>Tamanho</th>
+                        <!-- Adicione outras colunas conforme necessário -->
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    <tr>
+                        <td>{{ $produto->produto_nome }}</td>
+                        <td>{{ $produto->quantidade }}</td>
+                        <td>{{ $produto->tamanho }}</td>
+                        <!-- Adicione outras colunas conforme necessário -->
+                    </tr>
+                </tbody>
+
+            </table>
+
+        </div>
+
+    </div>
 
 @endsection
