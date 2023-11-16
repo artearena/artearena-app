c714fff89c797e14ce66815a3600f50e8825b21e
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-16 15:02:57 -0300

    test

--- resources/views/pages/listaUniformes/index.blade.php
+++ resources/views/pages/listaUniformes/index.blade.php
@@ -1,55 +1,73 @@
 @extends('layout.main')
 
-@section('title', 'Lista de informações dos produtos')
+@section('title', 'Lista de informações dos pedidos')
 
 @section('content')
 
     <div class="container">
 
-        <h1>Lista de informações dos produtos</h1>
+        <h1>Lista de informações dos pedidos</h1>
 
-        @if ($produtos)
-            <p>Total de produtos encontrados: {{ $produtos->count() }}</p>
+        @if ($pedidos)
+            <p>Total de pedidos encontrados: {{ $pedidos->count() }}</p>
 
-            <div class="divProduto">
+            <table class="table">
 
-                <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nome do cliente</th>
+                        <th>Número do pedido</th>
+                        <th>Produto</th>
+                        <th>Quantidade</th>
+                        <th>Sexo</th>
+                        <th>Arte aprovada</th>
+                        <th>Lista aprovada</th>
+                        <th>Pacote</th>
+                        <th>Camisa</th>
+                        <th>Calção</th>
+                        <th>Meia</th>
+                        <th>Nome</th>
+                        <th>Número</th>
+                        <th>Tamanho</th>
+                    </tr>
+                </thead>
 
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Número</th>
-                            <th>Tamanho</th>
-                        </tr>
-                    </thead>
+                <tbody>
 
-                    <tbody>
+                    @foreach($pedidos as $pedido)
 
-                        @foreach($produtos as $produto)
+                        {{-- Adicione verificação se $pedido é um objeto --}}
+                        @if (is_object($pedido))
 
-                            {{-- Adicione verificação se $produto é um objeto --}}
-                            @if (is_object($produto))
+                            <tr>
+                                <td>{{ $pedido->cliente_nome }}</td>
+                                <td>{{ $pedido->id }}</td>
+                                <td>{{ $pedido->produto_nome }}</td>
+                                <td>{{ $pedido->quantidade }}</td>
+                                <td>{{ $pedido->sexo }}</td>
+                                <td>{{ $pedido->arte_aprovada }}</td>
+                                <td>{{ $pedido->lista_aprovada }}</td>
+                                <td>{{ $pedido->pacote }}</td>
+                                <td>{{ $pedido->camisa }}</td>
+                                <td>{{ $pedido->calcao }}</td>
+                                <td>{{ $pedido->meiao }}</td>
+                                <td>{{ $pedido->nome }}</td>
+                                <td>{{ $pedido->numero }}</td>
+                                <td>{{ $pedido->tamanho }}</td>
+                            </tr>
 
-                                <tr>
-                                    <td>{{ $produto->produto_nome }}</td>
-                                    <td>{{ $produto->numero }}</td>
-                                    <td>{{ $produto->tamanho }}</td>
-                                </tr>
+                        @else
+                            <p>Pedido não é um objeto: {{ gettype($pedido) }}</p>
+                        @endif
 
-                            @else
-                                <p>Produto não é um objeto: {{ gettype($produto) }}</p>
-                            @endif
+                    @endforeach
 
-                        @endforeach
+                </tbody>
 
-                    </tbody>
-
-                </table>
-
-            </div>
+            </table>
 
         @else
-            <p>Nenhum produto encontrado.</p>
+            <p>Nenhum pedido encontrado.</p>
         @endif
 
     </div>
