f10bf2203eb3197b589e5a37f2c54ec14d2647ca
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-17 16:46:58 -0300

    att

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -11,7 +11,7 @@ class CadastroController extends Controller
     // Listar todos os registros
     public function index()
     {
-        return view('pages.cadastro.index', compact('registros', 'id'));
+        return view('pages.cadastro.index');
     }
     public function acessonegado()
     {
