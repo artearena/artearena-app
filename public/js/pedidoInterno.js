15a850c22e696100a457138794b3091bae7c6b62
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-23 13:32:52 -0300

    att

--- public/js/pedidoInterno.js
+++ public/js/pedidoInterno.js
@@ -109,7 +109,7 @@ function salvarPedido(pedidoId) {
 
       // Adicione os itens ajustados ao objeto pedido
       pedido.pedido.itens = itensAjustados;
-
+      console.log(itensAjustados);
       // Faça a solicitação para obter os dados do cliente usando a rota show
       fetch('/cadastro/show/' + pedidoId)
         .then(response => response.json())
