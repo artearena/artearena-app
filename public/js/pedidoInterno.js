bc3f7ba15dac0578c93282a1095506becd0fca72
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-24 15:12:06 -0300

    teste

--- public/js/pedidoInterno.js
+++ public/js/pedidoInterno.js
@@ -324,40 +324,44 @@ function shapeTable() {
     $('.dataTable td, .dataTable th').removeAttr('style');
   }
 }
+
+
 $(document).ready(function() {
   $('.btn-voltar-arte-final').click(function() {
-      var pedidoId = $(this).data('pedido-id');
-      Swal.fire({
-          title: "Alterar data?",
-          showCancelButton: true,
-          confirmButtonText: "Alterar",
-          cancelButtonText: "Manter",
-          html: '<input type="date" id="swal-date-picker" class="swal2-input">',
-          preConfirm: function() {
-              return document.getElementById('swal-date-picker').value;
+    var pedidoId = $(this).data('pedido-id');
+    Swal.fire({
+      title: "Alterar data?",
+      showCancelButton: true,
+      confirmButtonText: "Alterar",
+      cancelButtonText: "Manter",
+      html: '<input type="date" id="swal-date-picker" class="swal2-input">',
+      preConfirm: function() {
+        return document.getElementById('swal-date-picker').value;
+      }
+    }).then(function(result) {
+      if (result.isConfirmed) {
+        var data = result.value;
+        // Aqui você pode fazer a requisição AJAX para atualizar os dados no servidor
+        $.ajax({
+          url: '/pedido/' + pedidoId,
+          method: 'POST',
+          data: {
+            data: data,
+            "_token": "{{ csrf_token() }}"
+          },
+          success: function(response) {
+            // Aqui você pode tratar a resposta do servidor, se necessário
+            console.log(response);
+          },
+          error: function(xhr, status, error) {
+            // Aqui você pode tratar o erro, se necessário
+            console.log(xhr.responseText);
           }
-      }).then(function(result) {
-          if (result.isConfirmed) {
-              var data = result.value;
-              // Aqui você pode fazer a requisição AJAX para atualizar os dados no servidor
-              $.ajax({
-                  url: '/pedido/' + pedidoId,
-                  method: 'POST',
-                  data: {
-                      data: data
-                  },
-                  success: function(response) {
-                      // Aqui você pode tratar a resposta do servidor, se necessário
-                      console.log(response);
-                  },
-                  error: function(xhr, status, error) {
-                      // Aqui você pode tratar o erro, se necessário
-                      console.log(xhr.responseText);
-                  }
-              });
-          }
-      });
+        });
+      }
+    });
   });
+
   $('.table input').change(function() {
     var id = $(this).closest('tr').data('pedido-id');
     var observacoes = $(this).val();
@@ -366,7 +370,7 @@ $(document).ready(function() {
       method: 'PUT',
       data: {
         observacoes: observacoes,
-        "_token": "{{ csrf_token() }}",
+        "_token": "{{ csrf_token() }}"
       },
       success: function(response) {
         Swal.fire({
