c183928380280d3b399f837d3a657aedc4b77099
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-11-28 16:35:28 -0300

    att

--- resources/views/pages/trello/index.blade.php
+++ resources/views/pages/trello/index.blade.php
@@ -123,8 +123,7 @@
     }
     #seq_cards{
         display: flex;
-        align-items: center;
-        justify-content: space-between;
+        justify-content: space-around;
     }
     #relatorio #rcontainer {
         display: flex;
