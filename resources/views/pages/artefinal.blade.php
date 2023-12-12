d607b768adfdb39988ca3b6fa601ed09fb96f57f
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-12 11:33:25 -0300

    att

--- resources/views/pages/artefinal.blade.php
+++ resources/views/pages/artefinal.blade.php
@@ -862,49 +862,35 @@ $.ajaxSetup({
                 "_token": "{{ csrf_token() }}" // Adicione o token CSRF do Laravel para prevenir ataques CSRF
             },
             success: function(response) {
-                // Agora, você pode usar o ID do pedido para buscar mais detalhes
-                $.ajax({
-                    url: '/buscar-pedido?id=' + id,
-                    method: 'GET',
-                    success: function(response) {
-
-                    // Exemplo de como usar o ID do pedido
-                    console.log('resposta do pedido' + response);
-
-                    Swal.fire({
-                        title: 'Sucesso!',
-                        text: 'Pedido criado com sucesso. ID do pedido: ' + id,
-                        icon: 'success',
-                        timer: 3000,
-                        showConfirmButton: false
-                    });
-
-                    // Limpar os campos após o sucesso
-                    $('#id').val('');
-                    $('#data').val('');
-                    $('#material').val('');
-                    $('#medida_linear').val('');
-                    $('#observacoes').val('');
-                    $('#status').val('');
-                    $('#designer').val('');
-                    $('#link_trello').val('');
-                    $('#tipo_pedido').val('');
-                },
-
-                    error: function(xhr, status, error) {
-                        // Tratamento de erro ao buscar o pedido
-                        console.error('Erro ao buscar o pedido:', error);
-
-                        // Pedir ao usuário para reiniciar a página e cadastrar novamente
-                        Swal.fire({
-                            title: 'Erro!',
-                            text: 'Ocorreu um erro ao cadastrar o pedido. Por favor, reinicie a página e tente cadastrar novamente.',
-                            icon: 'error',
-                            showConfirmButton: true
-                        });
-                    }
+                Swal.fire({
+                    title: 'Sucesso!',
+                    text: 'Pedido criado com sucesso. Atualize a página para que o registro entre em vigor.',
+                    icon: 'success',
+                    timer: 3000, // Defina o tempo que o alerta será exibido (em milissegundos)
+                    showConfirmButton: false // Ocultar o botão "OK"
                 });
+
+                $('#id').val('');
+                $('#data').val('');
+                $('#material').val('');
+                $('#medida_linear').val('');
+                $('#observacoes').val('');
+                $('#status').val('');
+                $('#designer').val('');
+                $('#link_trello').val('');
+                $('#tipo_pedido').val('');
+
             },
+            error: function(xhr, status, error) {
+                // Tratamento de erro: exibir pop-up com a mensagem de erro filtrada
+                var errorMessage = "Erro ao criar pedido: ";
+                if (xhr.responseJSON && xhr.responseJSON.message) {
+                    errorMessage += filtrarMensagemErro(xhr.responseJSON.message);
+                } else {
+                    errorMessage += "Ocorreu um erro inesperado.";
+                }
+                alert(errorMessage);
+            }
         });
     });
     // Estilize o campo form-control com jQuery
