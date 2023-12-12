89ccc07c06d08873d2ef969cb319137edf262514
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-12 13:51:59 -0300

    att

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -87,8 +87,6 @@ class PedidoController extends Controller
         // Faça a lógica para buscar os dados do pedido com base no número fornecido
         $pedido = Pedido::where('id', $numeroPedido)->first();
         // Retorne os dados do pedido como uma resposta JSON
-        dd($numeroPedido);
-        dd($pedido);
         if (!$pedido) {
             return response()->json([
                 'error' => 'Pedido não encontrado'
