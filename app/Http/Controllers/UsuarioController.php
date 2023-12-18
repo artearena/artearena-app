7604ff22622dc757d37af4bad990b62aebae6411
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-18 17:03:13 -0300

    Update view paths in UsuarioController

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -10,12 +10,12 @@ class UsuarioController extends Controller
     public function index()
     {
         $usuarios = Usuario::all();
-        return view('usuarios.index', compact('usuarios'));
+        return view('pages.usuarios.index', compact('usuarios'));
     }
 
     public function create()
     {
-        return view('usuarios.create');
+        return view('pages.usuarios.create');
     }
 
     public function store(Request $request)
@@ -34,13 +34,13 @@ class UsuarioController extends Controller
     public function show($id)
     {
         $usuario = Usuario::find($id);
-        return view('usuarios.show', compact('usuario'));
+        return view('pages.usuarios.show', compact('usuario'));
     }
 
     public function edit($id)
     {
         $usuario = Usuario::find($id);
-        return view('usuarios.edit', compact('usuario'));
+        return view('pages.usuarios.edit', compact('usuario'));
     }
 
     public function update(Request $request, $id)
