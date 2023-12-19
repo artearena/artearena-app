755fb814a302b019b5d65d1c51aa55b12c41b8cd
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-19 14:55:34 -0300

    Refactor PermissaoController and routes/web.php

--- app/Http/Controllers/PermissaoController.php
+++ app/Http/Controllers/PermissaoController.php
@@ -4,78 +4,59 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Permissao;
-use App\Models\Tela;
 
 class PermissaoController extends Controller
 {
     public function index()
     {
         $permissoes = Permissao::all();
-        $Tela = Tela::all();
-
-        return view('pages.permissao.index', compact('permissoes','Tela'));
+        return view('pages.permissao.index', compact('permissoes'));
     }
 
     public function create()
     {
-        $Tela = Tela::all(); // Obtém todas as Tela do banco de dados
-
-        return view('pages.permissao.create', compact('Tela'));
+        // Lógica para carregar dados necessários, se necessário
+        $telas = Tela::all(); // Substitua Tela pelo nome correto do seu modelo de tela
+        return view('pages.permissao.create', compact('telas'));
     }
+
     public function store(Request $request)
-{
-    // Validação dos campos do formulário
-    $validatedData = $request->validate([
-        'nome' => 'required',
-        'configuracao_permissao' => 'nullable',
-    ]);
-
-    // Converter o array de IDs em uma string separada por vírgulas
-    $configuracaoPermissao = implode(',', $validatedData['configuracao_permissao']);
-
-    // Atualizar o campo 'configuracao_permissao' com a string de IDs
-    $validatedData['configuracao_permissao'] = $configuracaoPermissao;
-
-    // Criação da permissão no banco de dados
-    $permissao = Permissao::create($validatedData);
-
-    $Tela = Tela::all();
-    $permissoes = Permissao::all();
-
-    return view('pages.permissao', compact('Tela','permissoes'));
-
-}
-
-
-    public function edit(Permissao $permissao)
     {
-        return response()->json($permissao);
+        // Lógica para armazenar nova permissão
+        // Certifique-se de validar os dados do formulário
+        // e de tratar a configuração_permissao de acordo com sua estrutura
+
+        Permissao::create($request->all());
+
+        return redirect()->route('pages.permissao.index')->with('success', 'Permissão criada com sucesso.');
     }
 
-    public function update(Request $request, Permissao $permissao)
+    public function edit($id)
     {
-        // Validação dos campos do formulário
-        $validatedData = $request->validate([
-            'nome' => 'required',
-            'configuracao_permissao' => 'nullable',
-        ]);
-
-        // Atualização da permissão no banco de dados
-        $permissao->update($validatedData);
-
-        return response()->json([
-            'message' => 'Permissão atualizada com sucesso!',
-            'permissao' => $permissao
-        ], 200);
+        $permissao = Permissao::findOrFail($id);
+        // Lógica para carregar dados necessários, se necessário
+        $telas = Tela::all(); // Substitua Tela pelo nome correto do seu modelo de tela
+        return view('pages.permissao.edit', compact('permissao', 'telas'));
     }
 
-    public function destroy(Permissao $permissao)
+    public function update(Request $request, $id)
     {
+        // Lógica para atualizar permissão
+        // Certifique-se de validar os dados do formulário
+        // e de tratar a configuração_permissao de acordo com sua estrutura
+
+        $permissao = Permissao::findOrFail($id);
+        $permissao->update($request->all());
+
+        return redirect()->route('pages.permissao.index')->with('success', 'Permissão atualizada com sucesso.');
+    }
+
+    public function destroy($id)
+    {
+        // Lógica para excluir permissão
+        $permissao = Permissao::findOrFail($id);
         $permissao->delete();
 
-        $Tela = Tela::all();
-        $permissoes = Permissao::all();
-
-        return view('pages.permissao', compact('Tela','permissoes'));
+        return redirect()->route('pages.permissao.index')->with('success', 'Permissão excluída com sucesso.');
     }
 }
