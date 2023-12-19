cfa1f7c9015b6a655025a6e77556371f89f91769
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-19 14:40:29 -0300

    Add CheckPermissionsMiddleware to Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         //\App\Http\Middleware\CheckPermission::class,
-
+        \App\Http\Middleware\CheckPermissionsMiddleware::class,
     ];
 
     /**
@@ -38,7 +38,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            
+            \App\Http\Middleware\CheckPermissionsMiddleware::class,
+
         ],
 
         'api' => [
