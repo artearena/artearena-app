e44e7b80488d67dfe0a71f33339b38bac799c6a7
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-20 14:28:41 -0300

    Update middleware in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,17 +13,7 @@ class Kernel extends HttpKernel
      *
      * @var array<int, class-string|string>
      */
-    protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
-        \App\Http\Middleware\TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
-        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\VerificarPermissao::class,
 
-    ];
 
     /**
      * The application's route middleware groups.
@@ -68,6 +58,17 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'validar.token' => \App\Http\Middleware\ValidarToken::class,
         'perm.rota' => \App\Http\Middleware\VerificarPermissao::class,
+
+    ];
+    protected $middleware = [
+        // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\VerificarPermissao::class,
 
     ];
 }
