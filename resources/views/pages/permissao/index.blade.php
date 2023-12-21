8ca2c6ebbab5427c1c5f4195b47aa002de15bc79
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-21 14:34:38 -0300

    Remove unused jQuery script

--- resources/views/pages/permissao/index.blade.php
+++ resources/views/pages/permissao/index.blade.php
@@ -143,7 +143,7 @@
                 </div>
             </div>
         </div>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 <script>
     $('#modalAdicionarPermissao').on('shown.bs.modal', function (e) {
