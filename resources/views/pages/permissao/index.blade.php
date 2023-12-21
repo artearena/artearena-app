4f5693a6c46cbec01668bba51a060d1091f68ecd
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-21 15:01:08 -0300

    Update user authentication logic

--- resources/views/pages/permissao/index.blade.php
+++ resources/views/pages/permissao/index.blade.php
@@ -1,106 +1,84 @@
-    @extends('layout.main')
+@extends('layout.main')
 
-    @section('title')
+@section('title')
     Calculadora de Bandeiras
-    @endsection
+@endsection
 
-    @section('content')
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
-        <div class="container">
-            <h1>Administrar Permissões</h1>
+@section('content')
+    <div class="container">
+        <h1>Administrar Permissões</h1>
 
-            <div class="my-4">
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAdicionarPermissao">Adicionar Permissão</button>
-            </div>
-
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Configuração de Permissão</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($permissoes as $permissao)
-                        <tr>
-                            <td>{{ $permissao->id }}</td>
-                            <td>{{ $permissao->nome }}</td>
-                            <td>{{ $permissao->configuracao_permissao }}</td>
-                            <td>
-                                <!-- Botão Editar -->
-                                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalEditarPermissao{{ $permissao->id }}">
-                                    Editar
-                                </button>
-
-                                <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalConfirmarExclusao{{ $permissao->id }}">
-                                    Excluir
-                                </button>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="my-4">
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAdicionarPermissao">Adicionar Permissão</button>
         </div>
 
-        <!-- Modal Adicionar Permissão -->
-        <div class="modal fade" id="modalAdicionarPermissao" tabindex="-1" aria-labelledby="modalAdicionarPermissaoLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="modalAdicionarPermissaoLabel">Adicionar Permissão</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form action="{{ route('permissoes.store') }}" method="POST">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="nome" class="form-label">Nome</label>
-                                <input type="text" class="form-control" id="nome" name="nome" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="configuracao_permissao" class="form-label">Configuração de Permissão</label>
-                                <select class="form-control select2" id="configuracao_permissao" name="configuracao_permissao[]" multiple style="width: 100%">
-                                    @foreach ($telas as $tela)
-                                        @if ($tela->tipo == 'Não acessível' || $tela->tipo == 'Desativado')
-                                            @continue
-                                        @endif
-                                        <option value="{{ $tela->id }}">{{ $tela->nome_tela }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Configuração de Permissão</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($permissoes as $permissao)
+                    <tr>
+                        <td>{{ $permissao->id }}</td>
+                        <td>{{ $permissao->nome }}</td>
+                        <td>{{ $permissao->configuracao_permissao }}</td>
+                        <td>
+                            <!-- Botão Editar -->
+                            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalEditarPermissao{{ $permissao->id }}">
+                                Editar
+                            </button>
 
-                            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-                            <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
-                            <script>
-                                $('#modalAdicionarPermissao').on('shown.bs.modal', function (e) {
-                                    $('#configuracao_permissao').select2({
-                                        placeholder: 'Selecione as telas',
-                                        allowClear: true,
-                                        tags: true,
-                                        dropdownParent: $('#modalAdicionarPermissao') // Especifica o seletor do modal como o contêiner
-                                    });
-                                });
+                            <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalConfirmarExclusao{{ $permissao->id }}">
+                                Excluir
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
-                                $('#modalEditarPermissao{{ $permissao->id }}').on('shown.bs.modal', function (e) {
-                                    $('#configuracao_permissao').select2({
-                                        placeholder: 'Selecione as telas',
-                                        allowClear: true,
-                                        tags: true,
-                                        dropdownParent: $('#modalEditarPermissao{{ $permissao->id }}') // Especifica o seletor do modal como o contêiner
-                                    });
-                                });
-                            </script>
+    <!-- Modal Adicionar Permissão -->
+    <div class="modal fade" id="modalAdicionarPermissao" tabindex="-1" aria-labelledby="modalAdicionarPermissaoLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalAdicionarPermissaoLabel">Adicionar Permissão</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('permissoes.store') }}" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="nome" class="form-label">Nome</label>
+                            <input type="text" class="form-control" id="nome" name="nome" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="configuracao_permissao" class="form-label">Configuração de Permissão</label>
+                            <select class="form-control select2" id="configuracao_permissao" name="configuracao_permissao[]" multiple style="width: 100%">
+                                @foreach ($telas as $tela)
+                                    @if ($tela->tipo == 'Não acessível' || $tela->tipo == 'Desativado')
+                                        @continue
+                                    @endif
+                                    <option value="{{ $tela->id }}">{{ $tela->nome_tela }}</option>
+                                @endforeach
+                            </select>
+                        </div>
 
-                            <button type="submit" class="btn btn-primary">Salvar</button>
-                        </form>
-                    </div>
+                        <button type="submit" class="btn btn-primary">Salvar</button>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
 
-        <!-- Modal Editar Permissão -->
+    <!-- Modal Editar Permissão -->
+    @foreach ($permissoes as $permissao)
         <div class="modal fade" id="modalEditarPermissao{{ $permissao->id }}" tabindex="-1" aria-labelledby="modalEditarPermissaoLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -124,7 +102,7 @@
                                             @continue
                                         @endif
                                         <option value="{{ $tela->id }}" {{ in_array($tela->id, explode(',', $permissao->configuracao_permissao ?? '')) ? 'selected' : '' }}>{{ $tela->nome_tela }}</option>
-                                        @endforeach
+                                    @endforeach
                                 </select>
                             </div>
                             <button type="submit" class="btn btn-primary">Salvar</button>
@@ -133,8 +111,10 @@
                 </div>
             </div>
         </div>
+    @endforeach
 
-        <!-- Modal Confirmar Exclusão -->
+    <!-- Modal Confirmar Exclusão -->
+    @foreach ($permissoes as $permissao)
         <div class="modal fade" id="modalConfirmarExclusao{{ $permissao->id }}" tabindex="-1" aria-labelledby="modalConfirmarExclusaoLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -156,6 +136,29 @@
                 </div>
             </div>
         </div>
-    @endsection
+    @endforeach
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+    <script>
+        $('#modalAdicionarPermissao').on('shown.bs.modal', function (e) {
+            $('#configuracao_permissao').select2({
+                placeholder: 'Selecione as telas',
+                allowClear: true,
+                tags: true,
+                dropdownParent: $('#modalAdicionarPermissao') // Especifica o seletor do modal como o contêiner
+            });
+        });
 
+        @foreach ($permissoes as $permissao)
+            $('#modalEditarPermissao{{ $permissao->id }}').on('shown.bs.modal', function (e) {
+                $('#configuracao_permissao').select2({
+                    placeholder: 'Selecione as telas',
+                    allowClear: true,
+                    tags: true,
+                    dropdownParent: $('#modalEditarPermissao{{ $permissao->id }}') // Especifica o seletor do modal como o contêiner
+                });
+            });
+        @endforeach
+    </script>
+@endsection
