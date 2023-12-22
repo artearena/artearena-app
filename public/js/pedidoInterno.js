63ef833fd078502f350d991c6df63a6d65211542
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-22 13:49:40 -0300

    x

--- public/js/pedidoInterno.js
+++ public/js/pedidoInterno.js
@@ -4,84 +4,83 @@ var botoesExpandir = document.querySelectorAll('.btn-expand-produtos');
 // Adicione o evento de clique a todos os botões
 botoesExpandir.forEach(function(botaoExpandir) {
   botaoExpandir.addEventListener('click', function() {
-    // Obtenha a linha do pedido
-    var pedidoRow = this.closest('.pedido-row');
     // Obtenha o ID do pedido
-    var pedidoId = pedidoRow.getAttribute('data-pedido-id');
-    // Verifique se a linha de produtos já está visível
-    var produtosRow = document.querySelector('.produto-pedido-' + pedidoId);
+    var pedidoId = this.closest('.pedido-row').getAttribute('data-pedido-id');
 
-    if (produtosRow != null) {
-      console.log(produtosRow.style.display);
-      if (produtosRow.style.display === 'none') {
-        produtosRow.style.display = '';
-      } else {
-        produtosRow.style.display = 'none';
-      }
-    } else {
-
-      // Caso contrário, faça a requisição AJAX para obter a lista de produtos
-      fetch('/pedidoInterno/get-produtos-pedido/' + pedidoId)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(produtos) {
-          // Crie uma nova tabela para os produtos
-          var novaTabela = document.createElement('table');
-          novaTabela.classList.add('table', 'produto-pedido-' + pedidoId);
-
-          // Crie uma nova linha de cabeçalho
-          var cabecalho = novaTabela.createTHead();
-          var cabecalhoLinha = cabecalho.insertRow();
-          cabecalhoLinha.innerHTML = '<th>Produto</th><th>Quantidade</th><th>Sexo</th><th>Arte Aprovada</th><th>Lista Aprovada</th><th>Pacote</th><th>Camisa</th><th>Calção</th><th>Meião</th><th>Nome</th><th>Número</th><th>Tamanho</th><th>ID Lista</th>';
-
-          // Crie uma nova linha para cada produto
-          produtos.forEach(function(produto) {
-            var linhaProduto = novaTabela.insertRow();
-            linhaProduto.innerHTML =
-              '<td>' + (produto.produto_nome !== undefined ? produto.produto_nome : '') + '</td>' +
-              '<td>' + (produto.quantidade !== undefined ? produto.quantidade : '') + '</td>' +
-              '<td><select class="form-control input-sexo">' +
-              '<option value="M" ' + (produto.sexo === 'M' ? 'selected' : '') + '>M</option>' +
-              '<option value="F" ' + (produto.sexo === 'F' ? 'selected' : '') + '>F</option>' +
-              '</select></td>' +
-              '<td>' + (produto.arte_aprovada !== undefined ? produto.arte_aprovada : '') + '</td>' +
-              '<td><select class="form-control input-lista-aprovada">' +
-              '<option value="sim" ' + (produto.lista_aprovada === 'sim' ? 'selected' : '') + '>Sim</option>' +
-              '<option value="não" ' + (produto.lista_aprovada === 'não' ? 'selected' : '') + '>Não</option>' +
-              '</select></td>' +
-              '<td><select class="form-control input-pacote">' +
-              '<option value="start" ' + (produto.pacote === 'start' ? 'selected' : '') + '>Start</option>' +
-              '<option value="prata" ' + (produto.pacote === 'prata' ? 'selected' : '') + '>Prata</option>' +
-              '<option value="ouro" ' + (produto.pacote === 'ouro' ? 'selected' : '') + '>Ouro</option>' +
-              '<option value="diamante" ' + (produto.pacote === 'diamante' ? 'selected' : '') + '>Diamante</option>' +
-              '<option value="premium" ' + (produto.pacote === 'premium' ? 'selected' : '') + '>Premium</option>' +
-              '<option value="profissional" ' + (produto.pacote === 'profissional' ? 'selected' : '') + '>Profissional</option>' +
-              '</select></td>' +
-              '<td><input type="checkbox" ' + (produto.camisa !== undefined ? 'checked' : '') + ' class="input-camisa"></td>' +
-              '<td><input type="checkbox" ' + (produto.calcao !== undefined ? 'checked' : '') + ' class="input-calcao"></td>' +
-              '<td><input type="checkbox" ' + (produto.meiao !== undefined ? 'checked' : '') + ' class="input-meiao"></td>' +
-              '<td>' + (produto.nome !== undefined ? produto.nome : '') + '</td>' +
-              '<td>' + (produto.numero !== undefined ? produto.numero : '') + '</td>' +
-              '<td><select class="form-control input-tamanho">' +
-              '<option value="P" ' + (produto.tamanho === 'P' ? 'selected' : '') + '>P</option>' +
-              '<option value="M" ' + (produto.tamanho === 'M' ? 'selected' : '') + '>M</option>' +
-              '<option value="G" ' + (produto.tamanho === 'G' ? 'selected' : '') + '>G</option>' +
-              '<option value="GG" ' + (produto.tamanho === 'GG' ? 'selected' : '') + '>GG</option>' +
-              '<option value="XG" ' + (produto.tamanho === 'XG' ? 'selected' : '') + '>XG</option>' +
-              '<option value="XGG" ' + (produto.tamanho === 'XGG' ? 'selected' : '') + '>XGG</option>' +
-              '<option value="XGGG" ' + (produto.tamanho === 'XGGG' ? 'selected' : '') + '>XGGG</option>' +
-              '</select></td>' +
-              '<td>' + (produto.id_lista !== undefined ? produto.id_lista : '') + '</td>';
-          });
-
-          // Insira a nova tabela após a linha do pedido
-          pedidoRow.insertAdjacentElement('afterend', novaTabela);
-        })
-        .catch(function(error) {
-          console.log('Ocorreu um erro:', error);
+    // Caso contrário, faça a requisição AJAX para obter a lista de produtos
+    fetch('/pedidoInterno/get-produtos-pedido/' + pedidoId)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(produtos) {
+        // Construa o conteúdo do modal
+        var modalContent = '<div class="modal fade" id="produtoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">';
+        modalContent += '<div class="modal-dialog" role="document">';
+        modalContent += '<div class="modal-content">';
+        modalContent += '<div class="modal-header">';
+        modalContent += '<h5 class="modal-title" id="exampleModalLabel">Detalhes do Produto</h5>';
+        modalContent += '<button type="button" class="close" data-dismiss="modal" aria-label="Close">';
+        modalContent += '<span aria-hidden="true">&times;</span>';
+        modalContent += '</button>';
+        modalContent += '</div>';
+        modalContent += '<div class="modal-body">';
+        
+        // Adicione os detalhes dos produtos ao modal
+        produtos.forEach(function(produto) {
+          modalContent +=
+            '<strong>Produto:</strong> ' + (produto.produto_nome !== undefined ? produto.produto_nome : '') + '<br>' +
+            '<strong>Quantidade:</strong> ' + (produto.quantidade !== undefined ? produto.quantidade : '') + '<br>' +
+            '<strong>Sexo:</strong> <select class="form-control input-sexo">' +
+            '<option value="M" ' + (produto.sexo === 'M' ? 'selected' : '') + '>M</option>' +
+            '<option value="F" ' + (produto.sexo === 'F' ? 'selected' : '') + '>F</option>' +
+            '</select><br>' +
+            '<strong>Arte Aprovada:</strong> ' + (produto.arte_aprovada !== undefined ? produto.arte_aprovada : '') + '<br>' +
+            '<strong>Lista Aprovada:</strong> <select class="form-control input-lista-aprovada">' +
+            '<option value="sim" ' + (produto.lista_aprovada === 'sim' ? 'selected' : '') + '>Sim</option>' +
+            '<option value="não" ' + (produto.lista_aprovada === 'não' ? 'selected' : '') + '>Não</option>' +
+            '</select><br>' +
+            '<strong>Pacote:</strong> <select class="form-control input-pacote">' +
+            '<option value="start" ' + (produto.pacote === 'start' ? 'selected' : '') + '>Start</option>' +
+            '<option value="prata" ' + (produto.pacote === 'prata' ? 'selected' : '') + '>Prata</option>' +
+            '<option value="ouro" ' + (produto.pacote === 'ouro' ? 'selected' : '') + '>Ouro</option>' +
+            '<option value="diamante" ' + (produto.pacote === 'diamante' ? 'selected' : '') + '>Diamante</option>' +
+            '<option value="premium" ' + (produto.pacote === 'premium' ? 'selected' : '') + '>Premium</option>' +
+            '<option value="profissional" ' + (produto.pacote === 'profissional' ? 'selected' : '') + '>Profissional</option>' +
+            '</select><br>' +
+            '<strong>Camisa:</strong> <input type="checkbox" ' + (produto.camisa !== undefined ? 'checked' : '') + ' class="input-camisa">' + '<br>' +
+            '<strong>Calção:</strong> <input type="checkbox" ' + (produto.calcao !== undefined ? 'checked' : '') + ' class="input-calcao">' + '<br>' +
+            '<strong>Meião:</strong> <input type="checkbox" ' + (produto.meiao !== undefined ? 'checked' : '') + ' class="input-meiao">' + '<br>' +
+            '<strong>Nome:</strong> ' + (produto.nome !== undefined ? produto.nome : '') + '<br>' +
+            '<strong>Número:</strong> ' + (produto.numero !== undefined ? produto.numero : '') + '<br>' +
+            '<strong>Tamanho:</strong> <select class="form-control input-tamanho">' +
+            '<option value="P" ' + (produto.tamanho === 'P' ? 'selected' : '') + '>P</option>' +
+            '<option value="M" ' + (produto.tamanho === 'M' ? 'selected' : '') + '>M</option>' +
+            '<option value="G" ' + (produto.tamanho === 'G' ? 'selected' : '') + '>G</option>' +
+            '<option value="GG" ' + (produto.tamanho === 'GG' ? 'selected' : '') + '>GG</option>' +
+            '<option value="XG" ' + (produto.tamanho === 'XG' ? 'selected' : '') + '>XG</option>' +
+            '<option value="XGG" ' + (produto.tamanho === 'XGG' ? 'selected' : '') + '>XGG</option>' +
+            '<option value="XGGG" ' + (produto.tamanho === 'XGGG' ? 'selected' : '') + '>XGGG</option>' +
+            '</select><br>' +
+            '<strong>ID Lista:</strong> ' + (produto.id_lista !== undefined ? produto.id_lista : '') + '<hr>';
         });
-    }
+
+        modalContent += '</div>';
+        modalContent += '<div class="modal-footer">';
+        modalContent += '<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>';
+        modalContent += '</div>';
+        modalContent += '</div>';
+        modalContent += '</div>';
+        modalContent += '</div>';
+
+        // Inclua o modal no corpo do documento
+        document.body.insertAdjacentHTML('beforeend', modalContent);
+
+        // Ative o modal
+        $('#produtoModal').modal('show');
+      })
+      .catch(function(error) {
+        console.log('Ocorreu um erro:', error);
+      });
   });
 });
 
