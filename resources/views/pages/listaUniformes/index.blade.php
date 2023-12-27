3318f09ea2f2a7203d4e7d2d9fe24b70023a76f5
Arte Arena <138075749+artearena@users.noreply.github.com>
2023-12-27 12:38:03 -0300

    Refactor product filtering logic in index.blade.php

--- resources/views/pages/listaUniformes/index.blade.php
+++ resources/views/pages/listaUniformes/index.blade.php
@@ -24,25 +24,69 @@
         <tbody>
             @if ($produtos)
                 @foreach ($produtos as $produto)
-                    <tr>
-                        <td>{{ $produto->produto_nome }}</td>
-                        <td>{{ $produto->quantidade }}</td>
-                        <td>{{ $produto->preco_unitario }}</td>
-                        <td>{{ $produto->sexo }}</td>
-                        <td>{{ $produto->arte_aprovada }}</td>
-                        <td>{{ $produto->lista_aprovada }}</td>
-                        <td>{{ $produto->pacote }}</td>
-                        <td>{{ $produto->camisa }}</td>
-                        <td>{{ $produto->calcao }}</td>
-                        <td>{{ $produto->meiao }}</td>
-                        <td>{{ $produto->nome_jogador }}</td>
-                        <td>{{ $produto->numero }}</td>
-                        <td>{{ $produto->tamanho }}</td>
-                        <td>{{ $produto->id_lista }}</td>
-                    </tr>
+                    @php
+                        $nomeProdutoNormalizado = normalize($produto->produto_nome);
+                    @endphp
+
+                    @if (ehVestuario($nomeProdutoNormalizado))
+                        <tr>
+                            <td>{{ $produto->produto_nome }}</td>
+                            <td>{{ $produto->quantidade }}</td>
+                            <td>{{ $produto->preco_unitario }}</td>
+                            <td>{{ $produto->sexo }}</td>
+                            <td>{{ $produto->arte_aprovada }}</td>
+                            <td>{{ $produto->lista_aprovada }}</td>
+                            <td>{{ $produto->pacote }}</td>
+                            <td>{{ $produto->camisa }}</td>
+                            <td>{{ $produto->calcao }}</td>
+                            <td>{{ $produto->meiao }}</td>
+                            <td>{{ $produto->nome_jogador }}</td>
+                            <td>{{ $produto->numero }}</td>
+                            <td>{{ $produto->tamanho }}</td>
+                            <td>{{ $produto->id_lista }}</td>
+                        </tr>
+                    @endif
                 @endforeach
             @endif
-
         </tbody>
     </table>
+@endsection
+@section('extraScript')
+    @parent {{-- Mantenha qualquer conteúdo JavaScript existente --}}
+    <script>
+        function normalize(text) {
+            text = text.toLowerCase();
+            text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+            return text;
+        }
+
+        function ehVestuario(nomeProduto) {
+            var vestuario = [
+                "Camisa Personalizada", "Boné Premium Personalizado", "Chinelo Slide Personalizado",
+                "Chinelo Adulto Personalizado", "Abadá Personalizado", "Camiseta Personalizada",
+                "Toalha Personalizada", "Colete Personalizado", "Sacochila Personalizada",
+                "Braçadeira Personalizada", "Máscara Personalizada", "Máscara Caveira",
+                "Máscara Pikachu", "Máscara o Máskara", "Máscara La Casa de Papel",
+                "Máscara Homem Aranha", "Máscara Arlequina Coringa", "Máscara Et Alien",
+                "Máscara Girl Power", "Máscara Girl Power - Rosa", "Máscara Girl Power - Branco",
+                "Máscara Girl Power - Preto", "Máscara Good Vibes", "Máscara LGBT",
+                "Máscara Oncinha", "Máscara Resiliência", "Máscara Resiliência - Rosa",
+                "Máscara Resiliência - Preto", "Máscara Coringa", "Máscara Brasil",
+                "Samba Canção Personalizado", "Roupão Personalizado", "Doleira",
+                "Shorts Doll Personalizado", "Balaclava Personalizada"
+            ];
+
+            var nomeProdutoNormalizado = normalize(nomeProduto);
+
+            for (var i = 0; i < vestuario.length; i++) {
+                var item = normalize(vestuario[i]);
+
+                if (nomeProdutoNormalizado.includes(item)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    </script>
 @endsection
