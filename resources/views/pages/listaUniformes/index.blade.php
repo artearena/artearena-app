434d864329aab4905d31ebf966f3bbc3edbf7527
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-04 10:20:25 -0300

    Update URL for saving uniform list

--- resources/views/pages/listaUniformes/index.blade.php
+++ resources/views/pages/listaUniformes/index.blade.php
@@ -86,7 +86,7 @@
 
             $.ajax({
                 type: 'POST',
-                url: '/listaUniformes/salvarListaUniformes',
+                url: '/listaUniformes/gravarLista',
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 },
