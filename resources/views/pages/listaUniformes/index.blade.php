baf3ac34c65168789f6706bb20f92d06f2d58920
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-05 15:25:32 -0300

    Update size options and add new gola option

--- resources/views/pages/listaUniformes/index.blade.php
+++ resources/views/pages/listaUniformes/index.blade.php
@@ -127,18 +127,19 @@
                         var selectGola = row.querySelector('td:nth-child(12) select');
                         if (selectGola) {
                             selectGola.innerHTML = '<option value="Careca">Careca</option>';
+                            selectGola.innerHTML = '<option value="V">V</option>';
                         }
 
                         // As opções de tamanho serão M ou G
                         var selectTamanho = row.querySelector('td:nth-child(11) select');
                         selectTamanho.innerHTML = `
-                            <option value='P'>Médio (M)</option>
-                            <option value='M'>Grande (G)</option>    
-                            <option value='G'>Médio (M)</option>
-                            <option value='GG'>Grande (G)</option>
-                            <option value='XG'>Grande (G)</option>
-                            <option value='XXG'>Grande (G)</option>
-                            <option value='XXXG'>Grande (G)</option>
+                            <option value='P'>Pequeno (P)</option>
+                            <option value='M'>Médio (M)</option>    
+                            <option value='G'>Grande (G)</option>
+                            <option value='GG'>Muito Grande (GG)</option>
+                            <option value='XG'>Extra Grande (XG)</option>
+                            <option value='XXG'>Extra Grande 2 (XXG)</option>
+                            <option value='XXXG'>Extra Grande 3 (XXXG)</option>
                         `;
                         // Permitir nomes, sincronizar conforme digitado
                         var inputNomePrata = row.querySelector('td:nth-child(9) input');
