1c837470c83983f059fd63441394e2d85db6f78b
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-11 15:28:57 -0300

    Refactor ProducaoController to improve performance and readability

--- app/Http/Controllers/ProducaoController.php
+++ app/Http/Controllers/ProducaoController.php
@@ -14,30 +14,35 @@ class ProducaoController extends Controller
         // Busque todos os pedidos na etapa 'C'
         $pedidos = Pedido::where('etapa', 'C')->get();
 
-                // Para cada pedido, obtenha os produtos de confeccao associados
-        foreach ($pedidos as $pedido) {
-            // Supondo que haja uma relação entre Pedidos e ProdutoPedido através de um método chamado produtosPedido
-            $produtosPedido = $pedido->produtosPedido;
+        // Inicialize um array para armazenar os produtos relacionados
+        $produtos_confeccao = [];
 
-            // Verifique se $produtosPedido não é nulo antes de iterar sobre ele
-            if ($produtosPedido) {
-                // Para cada produtoPedido, obtenha o produto associado
-                foreach ($produtosPedido as $produtoPedido) {
-                    $produto = $produtoPedido->produto;
+        // Verifique se há pedidos na etapa 'C' antes de continuar
+        if ($pedidos->isNotEmpty()) {
+            // Para cada pedido, obtenha os produtos de confecção associados
+            foreach ($pedidos as $pedido) {
+                // Supondo que haja uma relação entre Pedidos e ProdutoPedido através de um método chamado produtosPedido
+                $produtosPedido = $pedido->produtosPedido;
 
-                    // Adicione o produto ao array
-                    $produtos_confeccao[] = $produto;
+                // Verifique se $produtosPedido não é nulo antes de iterar sobre ele
+                if ($produtosPedido) {
+                    // Para cada produtoPedido, obtenha o produto associado
+                    foreach ($produtosPedido as $produtoPedido) {
+                        $produto = $produtoPedido->produto;
+
+                        // Adicione o produto ao array
+                        $produtos_confeccao[] = $produto;
+                    }
                 }
             }
         }
-
 
         // Carregue os produtos de info
         $produtos_info = Produto::all();
 
         return view('pages.producao.index', compact('pedidos', 'produtos_info', 'produtos_confeccao'));
     }
-    
+
 
     public function create()
     {
