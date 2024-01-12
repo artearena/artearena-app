2df6cafc559069c603972ac7f9f4d9c1d53de491
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-12 16:13:12 -0300

    Update date inputs in relatorio.blade.php

--- resources/views/pages/tiny/relatorio.blade.php
+++ resources/views/pages/tiny/relatorio.blade.php
@@ -46,11 +46,11 @@
             <div class="row mb-3">
                 <div class="col-md-4">
                     <label for="dataInicial" class="form-label">Data Inicial</label>
-                    <input type="date" class="form-control" id="dataInicial" name="dataInicial">
+                    <input type="date" class="form-control" id="dataInicial" name="dataInicial" value="{{ date('Y-m-01') }}">
                 </div>
                 <div class="col-md-4">
                     <label for="dataFinal" class="form-label">Data Final</label>
-                    <input type="date" class="form-control" id="dataFinal" name="dataFinal">
+                    <input type="date" class="form-control" id="dataFinal" name="dataFinal" value="{{ date('Y-m-t') }}">
                 </div>
                 <div class="col-md-4">
                     <label for="situacao" class="form-label">Situação</label>
