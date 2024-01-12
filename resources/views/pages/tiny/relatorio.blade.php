62009e05348558855f85153df23b14b7ea60689e
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-12 16:20:28 -0300

    Refactor table structure and add conditional rendering for empty data

--- resources/views/pages/tiny/relatorio.blade.php
+++ resources/views/pages/tiny/relatorio.blade.php
@@ -63,29 +63,32 @@
         </form>
 
         <table class="mt-3">
-            <thead>
-                <tr>
-                    <th>Vendedor</th>
-                    <th>Soma Total em Reais</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($dados as $item)
-                    @if($item->soma_total_reais != 0)
-                        <tr>
-                            <td>{{ $item->nome_vendedor }}</td>
-                            <td>R$ {{ number_format($item->soma_total_reais, 2, ',', '.') }}</td>
-                        </tr>
-                    @endif
-                @endforeach
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td><strong>Total</strong></td>
-                    <td><strong>{{ $dados->soma_total_reais }}</strong></td>
-                </tr>
-            </tfoot>
-        </table>
+    <thead>
+        <tr>
+            <th>Vendedor</th>
+            <th>Soma Total em Reais</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse($somaTotalPorVendedor as $item)
+            <tr>
+                <td>{{ $item->nome_vendedor }}</td>
+                <td>R$ {{ number_format($item->soma_total_reais, 2, ',', '.') }}</td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="2">Nenhum dado encontrado</td>
+            </tr>
+        @endforelse
+    </tbody>
+    <tfoot>
+        <tr>
+            <td><strong>Total</strong></td>
+            <td><strong>R$ {{ number_format($somaTotalPorVendedor->sum('soma_total_reais'), 2, ',', '.') }}</strong></td>
+        </tr>
+    </tfoot>
+</table>
+
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
