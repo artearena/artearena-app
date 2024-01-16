5e4893b18114fa310f6764829b748ca77e1ae28b
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-16 09:42:24 -0300

    Fix permission check logic in VerificarPermissao middleware

--- app/Http/Middleware/VerificarPermissao.php
+++ app/Http/Middleware/VerificarPermissao.php
@@ -43,26 +43,26 @@ class VerificarPermissao
     {
         $cargo = $usuario->permissoes; // Certifique-se de ter o relacionamento entre Usuario e Cargo
 
-        if($cargo && $cargo == 36) {
+        if ($cargo && $cargo == 36) {
             return true;
         }
 
         if ($cargo) {
             // Obtém a lista de telas permitidas para o cargo
             $permissao = Permissao::find($cargo); // Supondo que $cargo seja o ID da permissão
-            
+
             if ($permissao) {
                 // Garante que $permissao->configuracao_permissao seja um array
                 $telasPermitidas = is_array($permissao->configuracao_permissao)
                     ? $permissao->configuracao_permissao
-                    : explode(',', $permissao->configuracao_permissao);
-        
+                    : explode(',', str_replace(['[', ']', '"'], '', $permissao->configuracao_permissao));
+
                 // Remove possíveis espaços em branco nos elementos do array
                 $telasPermitidas = array_map('trim', $telasPermitidas);
-                
+
                 // Obtém a lista de URLs correspondentes às telas permitidas
                 $urlsPermitidas = Tela::whereIn('id', $telasPermitidas)->pluck('url')->toArray();
-        
+
                 // Verifica se a urlCompleta está na lista de URLs permitidas
                 if (in_array($urlCompleta, $urlsPermitidas)) {
                     dd([
@@ -81,11 +81,10 @@ class VerificarPermissao
                 }
             }
         }
-        
-        
 
         return false;
     }
+
     
     
 }
