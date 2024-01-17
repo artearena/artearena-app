dde95eb0204415bce52b2801779548820b342915
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-17 16:49:41 -0300

    Refactor select statement in PedidoExterno model to include id_vendedor

--- app/Models/PedidoExterno.php
+++ app/Models/PedidoExterno.php
@@ -28,11 +28,11 @@ class PedidoExterno extends Model
 
     public static function obterSomaTotalPorVendedor($dataInicial, $dataFinal, $situacoes)
     {
-        return self::select('nome_vendedor', 'data_pedido')
+        return self::select('id_vendedor', 'nome_vendedor', 'data_pedido')
             ->selectRaw('CONCAT("R$ ", FORMAT(SUM(CASE WHEN situacao <> "Cancelado" THEN valor ELSE 0 END), 2)) AS soma_total_reais')
             ->whereBetween('data_pedido', [$dataInicial, $dataFinal])
             ->whereIn('situacao', $situacoes)
-            ->groupBy('nome_vendedor', 'data_pedido')
+            ->groupBy('id_vendedor', 'nome_vendedor', 'data_pedido')
             ->get();
     }
     
