1d1c6281ec79c657021d50e15acd35c10b826534
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-17 16:04:54 -0300

    Update chart.js library and remove unnecessary code

--- resources/views/pages/tiny/grafico.blade.php
+++ resources/views/pages/tiny/grafico.blade.php
@@ -7,49 +7,36 @@
     </div>
 </div>
 
-<!-- Adicione o Day.js antes do Chart.js -->
-<script src="https://cdn.jsdelivr.net/npm/dayjs@1.10.6"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.0"></script>
 
 <script>
-    // Configurar o adaptador de data para 'dayjs'
-    Chart.register({
-        id: 'dayjs',
-        beforeInit: function (chart, _options) {
-            chart.$adapter = Chart._adapters._date.adapters.dayjs;
-        },
-    });
-</script>
-
-<script>
-    // Recupere os dados do controlador e passe para o gráfico
     var labels = @json($labels);
     var data = @json($data);
 
     // Converta os dados de DateTime para Date usando 'dayjs'
     var convertedData = data.map(function (item) {
-        return dayjs(item).toDate();
+        return new Date(item);
     });
 
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
-        type: 'line', // Tipo de gráfico de linha do tempo
+        type: 'line',
         data: {
             labels: labels,
             datasets: [{
                 label: 'Total de Vendas por Vendedor',
                 data: convertedData,
-                fill: false, // Não preencher a área abaixo da linha
                 borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 2
+                borderWidth: 2,
+                fill: false,
             }]
         },
         options: {
             scales: {
                 x: {
-                    type: 'time', // Tipo de escala para o eixo X
+                    type: 'time',
                     time: {
-                        unit: 'day' // Ajuste conforme necessário (day, month, year, etc.)
+                        unit: 'day',
                     }
                 },
                 y: {
