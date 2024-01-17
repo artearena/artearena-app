3d745bcee73437efcd3c09c3c052a3c61e362300
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-17 17:35:37 -0300

    Refactor grafico.blade.php to include a container and row divs

--- resources/views/pages/tiny/grafico.blade.php
+++ resources/views/pages/tiny/grafico.blade.php
@@ -1,31 +1,36 @@
-<canvas id="myChart" width="400" height="200"></canvas>
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-md-12">
+            <canvas id="myChart" width="400" height="200"></canvas>
+        </div>
     </div>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script>
-        // Recupere os dados do controlador e passe para o gráfico
-        var labels = @json($labels);
-        var data = @json($data);
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    // Recupere os dados do controlador e passe para o gráfico
+    var labels = @json($labels);
+    var data = @json($data);
 
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Total de Vendas por Vendedor',
-                    data: data,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Total de Vendas por Vendedor',
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
                 }
             }
-        });
-    </script>
+        }
+    });
+</script>
