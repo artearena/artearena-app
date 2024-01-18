e477570bb61fe0781756ea8fd5006f1f1a46bcf9
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-18 15:44:34 -0300

    style

--- resources/views/pages/frete.blade.php
+++ resources/views/pages/frete.blade.php
@@ -16,7 +16,7 @@
   .form-group, .container, .card, .modal-content, .modal-body, .modal-title, .btn {
         border-radius: 8px; /* Bordas arredondadas para elementos */
         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Sombra suave para elementos */
-        margin-bottom: 15px; /* Espaçamento entre elementos */
+        margin-bottom: 5px; /* Espaçamento entre elementos */
   }
   .close {
     position: absolute;
