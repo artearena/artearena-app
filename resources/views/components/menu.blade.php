fa178aaacb723d3a5d556c896dd036573657c914
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-18 11:45:58 -0300

    Update profile picture size in menu component

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -82,7 +82,7 @@
                 <div style="display: flex; align-items: center; margin-right: 20px;">
                     @if(auth()->user()->foto_perfil)
                         <?php $base64Image = base64_encode(auth()->user()->foto_perfil); ?>
-                        <img src="{{ 'data:image/png;base64,'.$base64Image }}" alt="Foto de Perfil" style="width: 30px; height: 30px; border-radius: 50%; margin-right: 10px;">
+                        <img src="{{ 'data:image/png;base64,'.$base64Image }}" alt="Foto de Perfil" style="width: 50px; height: 50px; border-radius: 50%; margin-right: 10px;">
                     @endif
                     <div>
                         Bem-vindo, <b class="text-white">{{ auth()->user()->nome_usuario }}</b>
