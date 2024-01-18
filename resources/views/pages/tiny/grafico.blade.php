c1a54566c51cdc943dcf4831c38ad5eab690c85e
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-18 09:52:31 -0300

    Remove example data from grafico.blade.php

--- resources/views/pages/tiny/grafico.blade.php
+++ resources/views/pages/tiny/grafico.blade.php
@@ -11,8 +11,6 @@
     var labels = @json($meses);
     var data = @json($data);
     // Dados de exemplo
-    var labels = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",];
-    var data = [65, 59, 80, 81, 56, 55, 40];
 
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
