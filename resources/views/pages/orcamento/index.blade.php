309f9ed5972e835aa9e1ead947af0d232c444f38
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-19 13:58:42 -0300

    att

--- resources/views/pages/orcamento/index.blade.php
+++ resources/views/pages/orcamento/index.blade.php
@@ -25,7 +25,7 @@
 
         /* Adicione mais estilos conforme necessário */
     </style>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.6/css/jquery.dataTables.min.css">
+
 @endsection
 
 @section('content')
