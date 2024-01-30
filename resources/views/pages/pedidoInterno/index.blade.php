f38aa81903984ee927a2d830e8d870ffc56a7caa
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-01-30 13:34:52 -0300

    att

--- resources/views/pages/pedidoInterno/index.blade.php
+++ resources/views/pages/pedidoInterno/index.blade.php
@@ -236,25 +236,25 @@
         </table>
     </div>
     <div class="modal fade" id="modalListasUniforme" tabindex="-1" role="dialog" aria-labelledby="modalListasUniformeLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalListasUniformeLabel">Listas de Uniforme</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <ul class="nav nav-tabs" id="listasUniformeTabs" role="tablist">
-                    <!-- Abas serão inseridas dinamicamente aqui -->
-                </ul>
-                <div class="tab-content" id="listasUniformeContent">
-                    <!-- Conteúdo das abas serão inseridos dinamicamente aqui -->
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalListasUniformeLabel">Listas de Uniforme</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <ul class="nav nav-tabs" id="listasUniformeTabs" role="tablist">
+                        <!-- Abas serão inseridas dinamicamente aqui -->
+                    </ul>
+                    <div class="tab-content" id="listasUniformeContent">
+                        <!-- Conteúdo das abas serão inseridos dinamicamente aqui -->
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection
 @section('extraScript')
