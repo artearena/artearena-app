4e840fa9a2c466fff657460b9657600a08011035
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-02-02 15:14:54 -0300

    att

--- resources/views/pages/cadastro/index.blade.php
+++ resources/views/pages/cadastro/index.blade.php
@@ -5,13 +5,7 @@
 @section('content')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-<style>
-    .required-field::after {
-        content: "*";
-        color: red;
-        margin-left: 5px;
-    }
-</style>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -36,7 +30,7 @@
                         <div id="pessoa_juridica_campos" style="display: none;">
                             <div class="form-group">
                                 <label for="razao_social">Razão Social:</label>
-                                <input type="text" name="razao_social" id="razao_social" class="form-control required-field" required>
+                                <input type="text" name="razao_social" id="razao_social" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label for="cnpj">CNPJ:</label>
@@ -83,7 +77,7 @@
                         <div id="pessoa_fisica_campos" style="display: none;">
                             <div class="form-group">
                                 <label for="nome_completo">Nome Completo:</label>
-                                <input type="text" name="nome_completo" id="nome_completo" class="form-control required-field" required>
+                                <input type="text" name="nome_completo" id="nome_completo" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label for="rg">RG:</label>
@@ -103,19 +97,19 @@
                             </div>
                             <div class="form-group">
                                 <label for="endereco_fisica">Endereço:</label>
-                                <input type="text" name="endereco_fisica" id="endereco_fisica" class="form-control required-field" required>
+                                <input type="text" name="endereco_fisica" id="endereco_fisica" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label for="numero_fisica">N°:</label>
-                                <input type="text" name="numero_fisica" id="numero_fisica" class="form-control required-field" required>
+                                <input type="text" name="numero_fisica" id="numero_fisica" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label for="bairro_fisica">Bairro:</label>
-                                <input type="text" name="bairro_fisica" id="bairro_fisica" class="form-control required-field" required>
+                                <input type="text" name="bairro_fisica" id="bairro_fisica" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label for="cidade_fisica">Cidade:</label>
-                                <input type="text" name="cidade_fisica" id="cidade_fisica" class="form-control required-field" required>
+                                <input type="text" name="cidade_fisica" id="cidade_fisica" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label for="fone_fixo_fisica">Fone fixo:</label>
@@ -135,23 +129,23 @@
                         <div id="endereco_cobranca_campos" style="display: none;">
                             <div class="form-group">
                                 <label for="cep_cobranca">CEP:</label>
-                                <input type="text" name="cep_cobranca" id="cep_cobranca" class="form-control cep required-field" onblur="consultarCep('cobranca')" required>
+                                <input type="text" name="cep_cobranca" id="cep_cobranca" class="form-control cep" onblur="consultarCep('cobranca')" required>
                             </div>
                             <div class="form-group">
                                 <label for="endereco_cobranca">Endereço:</label>
-                                <input type="text" name="endereco_cobranca" id="endereco_cobranca" class="form-control required-field" required>
+                                <input type="text" name="endereco_cobranca" id="endereco_cobranca" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label for="numero_cobranca">N°:</label>
-                                <input type="text" name="numero_cobranca" id="numero_cobranca" class="form-control required-field" required>
+                                <input type="text" name="numero_cobranca" id="numero_cobranca" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label for="bairro_cobranca">Bairro:</label>
-                                <input type="text" name="bairro_cobranca" id="bairro_cobranca" class="form-control required-field" required>
+                                <input type="text" name="bairro_cobranca" id="bairro_cobranca" class="form-control" required>
                             </div>
                             <div class="form-group">
                                 <label for="cidade_cobranca">Cidade:</label>
-                                <input type="text" name="cidade_cobranca" id="cidade_cobranca" class="form-control required-field" required>
+                                <input type="text" name="cidade_cobranca" id="cidade_cobranca" class="form-control" required>
                             </div>
                         </div>
 
