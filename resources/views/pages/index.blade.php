cdbac0c8d4ab3dda5949611c448ff42d7878da9e
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-02-05 16:21:16 -0300

    att

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,6 @@ Inicio
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/date-holidays@3.23.7/+esm"></script>
 
 <div class="container">
     <div class="row">
