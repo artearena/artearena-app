7a66e9300770c77a069b0f62034cb613b59eb707
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-02-05 15:26:40 -0300

    att

--- resources/views/pages/logs/index.blade.php
+++ resources/views/pages/logs/index.blade.php
@@ -5,33 +5,38 @@
 @endsection
 
 @section('content')
-    <form method="GET" action="{{ route('logs.index') }}">
-        <input type="text" name="search" placeholder="Search logs" value="{{ request('search') }}">
-        <button type="submit">Search</button>
+    <form method="GET" action="{{ route('logs.index') }}" class="mb-4">
+        <div class="input-group">
+            <input type="text" name="search" class="form-control" placeholder="Search logs" value="{{ request('search') }}">
+            <div class="input-group-append">
+                <button type="submit" class="btn btn-primary">Search</button>
+            </div>
+        </div>
     </form>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID Pedido</th>
-                <th>Descrição</th>
-                <th>Tipo</th>
-                <th>Data</th>
-                <th>Atualizado por</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($logs as $log)
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
                 <tr>
-                    <td>{{ $log->id_pedido }}</td>
-                    <td>{{ $log->descricao }}</td>
-                    <td>{{ $log->tipo }}</td>
-                    <td>{{ $log->data }}</td>
-                    <td>{{ $log->usuario ? $log->usuario->nome_usuario : '' }}</td>
+                    <th>ID Pedido</th>
+                    <th>Descrição</th>
+                    <th>Tipo</th>
+                    <th>Data</th>
+                    <th>Atualizado por</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($logs as $log)
+                    <tr>
+                        <td>{{ $log->id_pedido }}</td>
+                        <td>{{ $log->descricao }}</td>
+                        <td>{{ $log->tipo }}</td>
+                        <td>{{ $log->data }}</td>
+                        <td>{{ $log->usuario ? $log->usuario->nome_usuario : '' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     {{ $logs->links("pagination::bootstrap-4") }}
-
 @endsection
