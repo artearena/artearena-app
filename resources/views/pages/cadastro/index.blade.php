468a8a48de9dfad893baaf6014b0a275b883424e
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-02-14 12:05:20 -0300

    att

--- resources/views/pages/cadastro/index.blade.php
+++ resources/views/pages/cadastro/index.blade.php
@@ -78,14 +78,18 @@
                                     </div>
                                 </div>
                                 <div class="row">
-                                    <div class="form-group">
-                                        <label for="endereco_juridica">Endereço:</label>
-                                        <input type="text" name="endereco_juridica" id="endereco_juridica" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="numero_juridica">N°:</label>
-                                        <input type="text" name="numero_juridica" id="numero_juridica" class="form-control">
-                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="endereco_juridica">Endereço:</label>
+                                            <input type="text" name="endereco_juridica" id="endereco_juridica" class="form-control">
+                                        </div>
+                                    <div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="numero_juridica">N°:</label>
+                                            <input type="text" name="numero_juridica" id="numero_juridica" class="form-control">
+                                        </div>
+                                    <div>
                                 </div>
                                 <div class="row">
                                         <div class="form-group">
