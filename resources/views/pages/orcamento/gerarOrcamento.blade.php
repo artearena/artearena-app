46d4893d608040fa5f7597cbaa344920247846a1
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-02-21 16:11:13 -0300

    att

--- resources/views/pages/orcamento/gerarOrcamento.blade.php
+++ resources/views/pages/orcamento/gerarOrcamento.blade.php
@@ -880,14 +880,14 @@
                   
                   // Adicionar desconto, se aplicável
                   if (desconto > 0) {
-                      valorTotal -= desconto;
+                      total -= desconto;
                       var descontoTxt = `Desconto: R$${desconto}\n`;
 
                   }
 
                   // Adicionar antecipação, se aplicável
                   if (antecipacao > 0) {
-                      valorTotal -= antecipacao;
+                      total -= antecipacao;
                       var antecipacaoTxt = `Antecipação: R$${antecipacao}\n`;
 
                   }
