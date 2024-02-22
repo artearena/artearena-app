068d8524871adc2d5ef3c0f5fb3904b0c7b78c24
Arte Arena <138075749+artearena@users.noreply.github.com>
2024-02-22 15:01:18 -0300

    att

--- resources/views/pages/pedido/consultaPedido.blade.php
+++ resources/views/pages/pedido/consultaPedido.blade.php
@@ -246,5 +246,32 @@
 
 @section('scripts')
 <script src="../js/etapas.js"></script>
+<script>
+$(document).ready(function() {
+    $("#oneAct").click(function() {
+        console.log('teste');
+        $("#oneAct").addClass("active");
+        $(".two, .three, .four, .five").removeClass("active");
+    });
 
+    $(".two").click(function() {
+        $(".one, .two").addClass("active");
+        $(".three, .four, .five").removeClass("active");
+    });
+
+    $(".three").click(function() {
+        $(".one, .two, .three").addClass("active");
+        $(".four, .five").removeClass("active");
+    });
+
+    $(".four").click(function() {
+        $(".one, .two, .three, .four").addClass("active");
+        $(".five").removeClass("active");
+    });
+
+    $(".five").click(function() {
+        $(".one, .two, .three, .four, .five").addClass("active");
+    });
+});
+</script>
 @endsection
